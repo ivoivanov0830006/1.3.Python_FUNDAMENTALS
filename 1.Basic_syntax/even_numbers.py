7e8e1bed0b96aead46544bb6758023e3efc925ab
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-14 23:10:38 +0300

    Create even_numbers.py

---
+++ 1.Basic_syntax/even_numbers.py
@@ -0,0 +1,25 @@
+total_numbers = int(input())
+
+for _ in range(total_numbers):
+    number = int(input())
+    if number % 2 == 0:
+        continue
+    else:
+        print(f"{number} is odd!")
+        break
+else:
+    print("All numbers are even.")
+
+    
+# Write a program that receives a number n on the first line. On the following n lines, 7
+# it receives different integer numbers. If the program receives an odd number,
+# print "{num} is odd!" and end the program. Otherwise, if all numbers given are even,
+# print "All numbers are even.".
+# Input	                    Output
+# 2
+# 12
+# 286	                    All numbers are even.
+# ------------------------------------------------------------
+# 5
+# 2
+# 9	                        9 is odd!
