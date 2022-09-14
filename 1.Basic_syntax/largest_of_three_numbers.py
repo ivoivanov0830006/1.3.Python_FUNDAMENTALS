86875eb1bc2ac88f22c18c6e7cb56078e0fc3cad
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-14 21:00:27 +0300

    Update and rename 1 to 1.Basic_syntax/largest_of_three_numbers.py

---
+++ 1.Basic_syntax/largest_of_three_numbers.py
@@ -0,0 +1,14 @@
+first_number, second_number, third_number = int(input()), int(input()), int(input())
+
+print(max(first_number, second_number, third_number))
+
+
+# Write a program that receives three whole numbers and prints the largest one.
+# Input	                Output
+# 3                     5
+# -1
+# 5
+# ----------------------------------------
+# 0                     0
+# -1
+# -2
