b7e7c4d6fac5549b4dbe9a5c916d3a1044055d53
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-14 21:40:23 +0300

    Update largest_of_three_numbers.py

--- 1.Basic_syntax/largest_of_three_numbers.py
+++ 1.Basic_syntax/largest_of_three_numbers.py
@@ -3,6 +3,21 @@ first_number, second_number, third_number = int(input()), int(input()), int(inpu
 print(max(first_number, second_number, third_number))
 
 
+# --------------------------- Another solution ----------------------------
+
+
+# first_num = int(input())
+# second_num = int(input())
+# third_num = int(input())
+#
+# if first_num > second_num and first_num > third_num:
+#     print(first_num)
+# elif second_num > first_num and second_num > third_num:
+#     print(second_num)
+# else:
+#     print(third_num)
+
+
 # Write a program that receives three whole numbers and prints the largest one.
 # Input	                Output
 # 3                     5
