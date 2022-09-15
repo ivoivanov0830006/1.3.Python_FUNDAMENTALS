a53c22b0069eb4082ad02b5fe614d6de08fd39ef
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-15 20:55:27 +0300

    Create pattern.py

---
+++ 1.Basic_syntax/pattern.py
@@ -0,0 +1,18 @@
+number = int(input())
+for i in range(1, number + 1):
+    print(i * "*")
+for j in range(number - 1, -1, -1):
+    print(j * "*")
+
+# Write a program that receives a number and creates the following pattern.
+# The number represents the largest count of stars on one row.
+#
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
