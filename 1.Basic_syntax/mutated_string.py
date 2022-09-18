bd3396aff00e6e4b5d29a10b3dbbac9bab659865
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-18 19:37:16 +0300

    Update mutated_string.py

--- 1.Basic_syntax/mutated_string.py
+++ 1.Basic_syntax/mutated_string.py
@@ -14,3 +14,17 @@ for char in range(len(word_1)):
     else:
         last_word = ""
         continue
+
+# You will be given two strings. Transform the first string into the second one, letter by letter,
+# starting from the first one. After each interaction, print the resulting string only if it is unique.
+# Note: the strings will have the same length.
+# Input	                Output
+# bubble gu             tubble gum
+# turtle hum            turble gum
+#                       turtle gum
+#                       turtle hum
+# ---------------------------------------------
+# Kitty                 Ditty
+# Doggy	                Dotty
+#                       Dogty
+#                       Doggy
