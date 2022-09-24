53e83a48ec06a7aae8de089a068d6a29c701260b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-24 22:14:41 +0300

    Update happy_new_year.py

--- 2.Data_types_and_variables/happy_new_year.py
+++ 2.Data_types_and_variables/happy_new_year.py
@@ -13,6 +13,17 @@ while not unique_year:
 
 print(current_year)
 
+# --------------------------- Another solution ----------------------------
+#
+# from itertools import permutations
+# number = tuple(map(int, input()))
+# mypermutation = permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], len(number))
+#
+# for digits in list(mypermutation):
+#     if digits > number:
+#         result = "".join(str(x) for x in digits)
+#         print(result)
+#         break
 
 # You are saying goodbye to your best friend: "See you next happy year".
 # Happy Year is the year with only distinct digits, for example, 2018.
