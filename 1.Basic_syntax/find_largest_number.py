3ae569f2f8044c30451ec4154891d6b5d0856353
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-24 22:22:31 +0300

    Update find_largest_number.py

--- 1.Basic_syntax/find_largest_number.py
+++ 1.Basic_syntax/find_largest_number.py
@@ -2,6 +2,9 @@ number = list(input())
 number.sort(reverse=True)
 print(''.join(number))
 
+
+# --------------------------- Problem to resolve ----------------------------
+#
 # You will be given a number. Print the largest number that can be formed from the digits of the given number.
 # Input	        Output
 # 213	        321
