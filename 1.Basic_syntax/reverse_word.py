5693aeea84afecf84855446a164925db60e4bf94
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-24 22:21:22 +0300

    Update reverse_word.py

--- 1.Basic_syntax/reverse_word.py
+++ 1.Basic_syntax/reverse_word.py
@@ -4,14 +4,14 @@ print(word[::-1])
 
 
 # --------------------------- Another solution ----------------------------
-
+#
 # word = input()
 #
 # reversed_word = ""
 # for i in range(len(word) - 1, -1, -1):
 #     reversed_word += word[i]
 # print(reversed_word)
-
+#
 # --------------------------- Problem to resolve ----------------------------
-
+#
 # Write a program that receives a single word, reverses it, and prints it.
