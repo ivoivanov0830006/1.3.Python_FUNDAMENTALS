ac34009055c630759a927c5b63cd26a399735454
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-24 22:27:45 +0300

    Update pattern.py

--- 1.Basic_syntax/pattern.py
+++ 1.Basic_syntax/pattern.py
@@ -3,7 +3,9 @@ for i in range(1, number + 1):
     print(i * "*")
 for j in range(number - 1, -1, -1):
     print(j * "*")
-
+    
+# --------------------------- Problem to resolve ----------------------------
+#
 # Write a program that receives a number and creates the following pattern.
 # The number represents the largest count of stars on one row.
 #
