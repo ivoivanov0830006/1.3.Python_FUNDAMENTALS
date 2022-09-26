a41ce70e05c308472d1220fbc88ac90bfb07658e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-26 23:13:20 +0300

    Update triple_latin_letters_from_ASCII.py

--- 2.Data_types_and_variables/triple_latin_letters_from_ASCII.py
+++ 2.Data_types_and_variables/triple_latin_letters_from_ASCII.py
@@ -6,3 +6,46 @@ for i in range(start, end):
     for j in range(start, end):
         for k in range(start, end):
             print(f"{chr(i)}{chr(j)}{chr(k)}")
+            
+
+# --------------------------- Problem to resolve ----------------------------
+
+# Write a program to read an integer N and print all triples of the first N small
+# Latin letters, ordered alphabetically:
+# Input	            Output
+# 3	                aaa
+#                   aab
+#                   aac
+#                   aba
+#                   abb
+#                   abc
+#                   aca
+#                   acb
+#                   acc
+#                   baa
+#                   bab
+#                   bac
+#                   bba
+#                   bbb
+#                   bbc
+#                   bca
+#                   bcb
+#                   bcc
+#                   caa
+#                   cab
+#                   cac
+#                   cba
+#                   cbb
+#                   cbc
+#                   cca
+#                   ccb
+#                   ccc
+# --------------------------------------
+# 2	                aaa
+#                   aab
+#                   aba
+#                   abb
+#                   baa
+#                   bab
+#                   bba
+#                   bbb
