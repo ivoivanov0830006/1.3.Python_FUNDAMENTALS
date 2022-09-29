081d5ceabd94d58bf5786cd317ba4e363e721ee0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-29 22:49:21 +0300

    Update numbers_filter.py

--- 3.Lists/numbers_filter.py
+++ 3.Lists/numbers_filter.py
@@ -27,3 +27,33 @@ for number in numbers:
         filtered_numbers.append(number)
 
 print(filtered_numbers)
+
+
+# --------------------------- Another solution ----------------------------
+#
+# number = int(input())
+# numbers = []
+# filtered = []
+#
+# for _ in range(number):
+#     current_number = int(input())
+#     numbers.append(current_number)
+# command = input()
+# if command == "even":
+#     for number in numbers:
+#         if number % 2 == 0:
+#             filtered.append(number)
+# elif command == "odd":
+#     for number in numbers:
+#         if number % 2 != 0:
+#             filtered.append(number)
+# elif command == "negative":
+#     for number in numbers:
+#         if number < 0:
+#             filtered.append(number)
+# elif command == "positive":
+#     for number in numbers:
+#         if number >= 0:
+#             filtered.append(number)
+# print(filtered)
+
