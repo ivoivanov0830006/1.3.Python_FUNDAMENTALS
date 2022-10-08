b295862379bb4f3b24ee2b8bf0c7654a2c9b9c2b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-08 17:39:10 +0300

    Update repeat_string.py

--- 4.Functions/repeat_string.py
+++ 4.Functions/repeat_string.py
@@ -5,3 +5,10 @@ def repeat_string(current_string, num):
 string = input()
 number = int(input())
 print(repeat_string(string, number))
+
+# ------------------------------------- Another Solution -----------------------------
+
+# string = input()
+# number = int(input())
+# result = lambda str, num: str * num
+# print(result(string, number))
