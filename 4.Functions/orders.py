ddbaeff327a6bd6b79524033cf818f459db4c207
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-08 17:37:34 +0300

    Update orders.py

--- 4.Functions/orders.py
+++ 4.Functions/orders.py
@@ -17,3 +17,28 @@ coke_price = 1.4
 snacks_price = 2
 total = orders(current_order)
 print(f"{total:.2f}")
+
+# ------------------------------------- Another solution -------------------------------
+
+# def orders(order, quantity):
+#     price = 0
+#     coffee_price = 1.5
+#     water_price = 1
+#     coke_price = 1.4
+#     snacks_price = 2
+#     if order == "coffee":
+#         price = quantity * coffee_price
+#     elif order == "water":
+#         price = quantity * water_price
+#     elif order == "coke":
+#         price = quantity * coke_price
+#     elif order == "snacks":
+#         price = quantity * snacks_price
+#     return price
+#
+#
+# current_order = input()
+# current_quantity = int(input())
+#
+# total = orders(current_order, current_quantity)
+# print(f"{total:.2f}")
