bb17316c5f5ce2ff74c90a3a506b53ae4649c762
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-09 23:13:06 +0300

    Update factorial_division.py

--- 4.Functions/factorial_division.py
+++ 4.Functions/factorial_division.py
@@ -12,3 +12,15 @@ number_1 = int(input())
 number_2 = int(input())
 
 print(end_result(factorial(number_1), factorial(number_2)))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a function that receives two integer numbers. Calculate the factorial of each number.
+# Divide the first result by the second and print the division formatted to the second decimal point.
+# Input	                        Output
+# 5                             60.00
+# 2
+# ---------------------------------------
+# 6                             360.00
+# 2
