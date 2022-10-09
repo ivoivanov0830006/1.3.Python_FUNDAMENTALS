bc9a817bc4ac96ccbd5a44e41c96f3e421acc98d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-09 23:33:28 +0300

    Create password.py

---
+++ 4.Functions/password.py
@@ -0,0 +1,25 @@
+def login(input_pass):
+    number_count = 0
+    is_valid = True
+    for char in input_pass:
+        symbol = ord(char)
+        if 48 <= symbol <= 57:
+            number_count += 1
+        if symbol < 48 or 57 < symbol < 65 or 90 < symbol < 97 or symbol > 122:
+            is_valid = False
+            print("Password must consist only of letters and digits")
+            break
+    if 10 < len(input_pass) or len(input_pass) < 6:
+        is_valid = False
+        print("Password must be between 6 and 10 characters")
+
+    if number_count < 2:
+        is_valid = False
+        print("Password must have at least 2 digits")
+    if is_valid:
+        print("Password is valid")
+
+
+password = input()
+login(password)
+
