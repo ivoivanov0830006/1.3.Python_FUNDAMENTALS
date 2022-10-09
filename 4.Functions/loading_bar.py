7b92fe420cc8913b4edefd6ff6d24c724b2fdd59
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-09 23:14:02 +0300

    Update loading_bar.py

--- 4.Functions/loading_bar.py
+++ 4.Functions/loading_bar.py
@@ -27,3 +27,18 @@ loading(input_percentage)
 #
 # print(loading(input_percentage))
 #
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will receive a single integer number between 0 and 100 (inclusive) divisible by 10 without
+# remainder (0, 10, 20, 30...). Your task is to create a function that returns a loading bar depending
+# on the number you have received in the input. Print the result on the console. For more clarification,
+# see the examples below.
+# Input	                        Output
+# 30	                        30% [%%%.......]
+#                               Still loading...
+# -------------------------------------------------
+# 50	                        50% [%%%%%.....]
+#                               Still loading...
+# -------------------------------------------------
+# 100	                        100% Complete!
+#                               [%%%%%%%%%%]
