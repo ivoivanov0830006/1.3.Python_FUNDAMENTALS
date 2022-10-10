e1a978db9fe90a2d22fa19f1d943e95321761208
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-10 20:21:57 +0300

    Update add_and_subtract.py

--- 4.Functions/add_and_subtract.py
+++ 4.Functions/add_and_subtract.py
@@ -15,3 +15,24 @@ num_3 = int(input())
 total = subtract((sum_numbers(num_1, num_2)), num_3)
 print(total)
 
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will receive three integer numbers.
+# Write functions named:
+# sum_numbers() that returns the sum of the first two integers
+# subtract() that returns the difference between the returned result of the first function and the third
+# integer. Wrap the two functions in a function named add_and_subtract() which will receive the three
+# numbers as parameters. Print the result of the subtract() function on the console.
+# Input	                            Output
+# 23                                19
+# 6
+# 10
+# -------------------------------------------------
+# 1                                 -12
+# 17
+# 30
+# -------------------------------------------------
+# 42                                0
+# 58
+# 100
