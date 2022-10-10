f139481ef5e8bf0252bb28d29969883825d0977b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-10 20:02:54 +0300

    Update password.py

--- 4.Functions/password.py
+++ 4.Functions/password.py
@@ -23,3 +23,28 @@ def login(input_pass):
 password = input()
 login(password)
 
+# ------------------------------------- Another Solution -----------------------------
+#
+# def login(input_pass):
+#     number_count = 0
+#     is_valid = True
+#     if len(input_pass) < 6 or len(input_pass) > 10:
+#         print("Password must be between 6 and 10 characters")
+#         is_valid = False
+#     if not input_pass.isalnum():
+#         print("Password must consist only of letters and digits")
+#         is_valid = False
+#     for char in input_pass:
+#         if char.isdigit():
+#             number_count += 1
+#     if number_count < 2:
+#         print("Password must have at least 2 digits")
+#         is_valid = False
+#     return is_valid
+#
+#
+# password = input()
+# password_is_valid = login(password)
+# if password_is_valid:
+#     print("Password is valid")
+#
