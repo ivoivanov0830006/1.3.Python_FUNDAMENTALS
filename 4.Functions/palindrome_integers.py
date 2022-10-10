7021374a7d52409203c56857f2f9fde906a88600
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-10 20:08:07 +0300

    Update palindrome_integers.py

--- 4.Functions/palindrome_integers.py
+++ 4.Functions/palindrome_integers.py
@@ -11,3 +11,19 @@ def palindrome(num_list):
 
 numbers_list = list(input().split(", "))
 palindrome(numbers_list)
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# A palindrome is a number that reads the same backward as forward, such as 323 or 1001.
+# Write a function that receives a list of positive integers, separated by comma and space ", ".
+# The function should check if each integer is a palindrome - True or False. Print the result.
+# Input	                        Output
+# 123, 323, 421, 121	        False
+#                               True
+#                               False
+#                               True
+# 32, 2, 232, 1010	            False
+#                               True
+#                               True
+#                               False
+
