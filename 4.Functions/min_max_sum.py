d1334586f37a803aeea89434184b363d33466853
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-10 20:12:43 +0300

    Update min_max_sum.py

--- 4.Functions/min_max_sum.py
+++ 4.Functions/min_max_sum.py
@@ -22,3 +22,22 @@ total_sum = sum_all_num(numbers_list)
 print(f"The minimum number is {minimum_number}")
 print(f"The maximum number is {maximum_number}")
 print(f"The sum number is: {total_sum}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that receives a sequence of numbers (integers) separated by a single space.
+# It should print the min and max values of the given numbers and the sum of all the numbers in the list.
+# Use min(), max() and sum().
+# The output should be as follows:
+#   - On the first line: "The minimum number is {minimum number}"
+#   - On the second line: "The maximum number is {maximum number}"
+#   - On the third line: "The sum number is: {sum of all numbers}"
+# Input	                    Output
+# 2 4 6	                    The minimum number is 2
+#                           The maximum number is 6
+#                           The sum number is: 12
+# -----------------------------------------------------
+# 12 52 11 53 2 8 45	    The minimum number is 2
+#                           The maximum number is 53
+#                           The sum number is: 183
