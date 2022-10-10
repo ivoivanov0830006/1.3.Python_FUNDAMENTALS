5e583c78926bc46b3b3a41bb6a0f443df7739ff1
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-10 20:15:43 +0300

    Update sorted_numbers.py

--- 4.Functions/sorted_numbers.py
+++ 4.Functions/sorted_numbers.py
@@ -1,3 +1,13 @@
 numbers_list = list(map(int, input().split(" ")))
 numbers_sorted = sorted(numbers_list)
 print(numbers_sorted)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that receives a sequence of numbers (integers) separated by a single space.
+# It should print a sorted list of numbers in ascending order. Use sorted().
+# Input         	            Output
+# 6 2 4	                        [2, 4, 6]
+# ---------------------------------------------------------
+# 12 52 11 53 2 8 45	        [2, 8, 11, 12, 45, 52, 53]
