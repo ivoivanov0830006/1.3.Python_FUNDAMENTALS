eeb9124d9481469f30c91bc3099065af08789443
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-11 23:24:54 +0300

    Create multiples_list.py

---
+++ 3.Lists/multiples_list.py
@@ -0,0 +1,11 @@
+factor = int(input())
+count = int(input())
+start = factor
+end = factor * count
+
+new_list = []
+
+for number in range(start, end + 1):
+    if number % factor == 0:
+        new_list.append(number)
+print(new_list)
