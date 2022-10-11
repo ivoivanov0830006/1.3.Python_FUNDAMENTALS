56f76ebcf370b97d132c5db81bdbe0a66b8a9477
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-11 23:25:44 +0300

    Update multiples_list.py

--- 3.Lists/multiples_list.py
+++ 3.Lists/multiples_list.py
@@ -10,3 +10,26 @@ for number in range(start, end + 1):
         new_list.append(number)
 print(new_list)
 
+# --------------------------- Another solution ----------------------------
+#
+# factor = int(input())
+# count = int(input())
+# new_list = []
+# number = 0
+#
+# for _ in range(1, count + 1):
+#     number += factor
+#     new_list.append(int(number))
+#
+# print(new_list)
+#
+# --------------------------- Another solution ----------------------------
+#
+# factor = int(input())
+# count = int(input())
+#
+# new_list = []
+#
+# for multiplier in range(1, count + 1):
+#     new_list.append(factor * multiplier)
+# print(new_list)
