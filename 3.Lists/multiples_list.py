0012ace7679635aed06330aa775d92b7e5e83bd0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-11 23:26:01 +0300

    Update multiples_list.py

--- 3.Lists/multiples_list.py
+++ 3.Lists/multiples_list.py
@@ -33,3 +33,16 @@ print(new_list)
 # for multiplier in range(1, count + 1):
 #     new_list.append(factor * multiplier)
 # print(new_list)
+#
+# --------------------------- Problem to resolve -----------------------------
+#
+# Write a program that receives two numbers (factor and count). It should create a list with a length
+# of the given count that contains only integer numbers, which are multiples of the given factor.
+# The numbers should be only positive, and they should be arranged in ascending order, starting from
+# the value of the factor.
+# Input	                    Output
+# 2                         [2, 4, 6, 8, 10]
+# 5
+# --------------------------------------------------
+# 1                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# 10
