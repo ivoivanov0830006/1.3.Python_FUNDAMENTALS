9e82b5e72730ec49a4de23f96dd5a0787ea12cf9
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-11 23:28:13 +0300

    Update inverted_values.py

--- 3.Lists/inverted_values.py
+++ 3.Lists/inverted_values.py
@@ -8,3 +8,12 @@ for string in numbers_list:
     negative_numbers_list.append(negative_number)
 
 print(negative_numbers_list)
+
+# --------------------------- Problem to resolve -----------------------------
+#
+# Write a program that receives a single string containing positive and negative numbers
+# separated by a single space. Print a list containing the opposite of each number.
+# Input	                                Output
+# 1 2 -3 -3 5	                        [-1, -2, 3, 3, -5]
+# ------------------------------------------------------------
+# -4 0 2 57 -101	                    [4, 0, -2, -57, 101]
