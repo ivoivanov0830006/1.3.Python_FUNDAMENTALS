08ec10c1a67a96776e9c2fee60fa9a85946b50eb
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-11 23:23:24 +0300

    Update inverted_values.py

--- 3.Lists/inverted_values.py
+++ 3.Lists/inverted_values.py
@@ -6,3 +6,5 @@ numbers_list = list(numbers.split(" "))
 for string in numbers_list:
     negative_number = -int(string)
     negative_numbers_list.append(negative_number)
+
+print(negative_numbers_list)
