b1097d999f9c09c530361df199507744aabe2bf6
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-11 23:25:11 +0300

    Update multiples_list.py

--- 3.Lists/multiples_list.py
+++ 3.Lists/multiples_list.py
@@ -9,3 +9,16 @@ for number in range(start, end + 1):
     if number % factor == 0:
         new_list.append(number)
 print(new_list)
+
+# --------------------------- Another solution ----------------------------
+#
+# factor = int(input())
+# count = int(input())
+# new_list = []
+# number = 0
+#
+# for _ in range(1, count + 1):
+#     number += factor
+#     new_list.append(int(number))
+#
+# print(new_list)
