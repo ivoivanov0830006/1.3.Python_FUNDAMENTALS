b62ec2a21a014508e9a990a03bbfa60c7666f8d5
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-13 21:37:26 +0300

    Update train_wagons.py

--- 5.Lists_advanced/train_wagons.py
+++ 5.Lists_advanced/train_wagons.py
@@ -21,3 +21,22 @@ while True:
         train[index] -= people
 
 print(train)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will receive a number representing the number of wagons a train has. Create a list (train) with
+# the given length containing only zeros. Until you receive the command "End", you will receive some of
+# the following commands:
+#   · "add {people}" – you should add the number of people in the last wagon
+#   · "insert {index} {people}" - you should add the number of people at the given wagon
+#   · "leave {index} {people}" - you should remove the number of people from the wagon. There will be
+# no case in which the people will be more than the count in the wagon.
+# There will be no case in which the index is invalid!
+# After you receive the "End" command print the train.
+# Input                             Output
+# 3                                 [10, 0, 20]
+# add 20
+# insert 0 15
+# leave 0 5
+# End
