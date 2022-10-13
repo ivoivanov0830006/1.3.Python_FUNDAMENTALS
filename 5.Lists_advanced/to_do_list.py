02fd3ce649d80df2d37348a368b66e05a9d417de
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-13 23:57:31 +0300

    Update to_do_list.py

--- 5.Lists_advanced/to_do_list.py
+++ 5.Lists_advanced/to_do_list.py
@@ -16,3 +16,27 @@ for lst in sorted_list:
     char = lst.pop(1)
     final_list.append(char)
 print(final_list)
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+#
+# tasks = []
+#
+# while True:
+#     command = input()
+#     if command == "End":
+#         break
+#     split_command = command.split("-")
+#     current_priority = int(split_command[0])
+#     current_task = split_command[1]
+#     tasks.append([current_priority, current_task])
+#
+# # -------------------------------------------------------------
+# # sorted_tasks = [task_data[1] for task_data in sorted(tasks)]
+# sorted_tasks = []
+# for task_data in sorted(tasks):
+#     sorted_tasks.append(task_data[1])
+#
+# print(sorted_tasks)
+
