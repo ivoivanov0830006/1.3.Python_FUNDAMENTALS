c03383fdf9c795f497e0dc4d81538aa1de95ce8c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-13 20:44:21 +0300

    Update no_vowels.py

--- 5.Lists_advanced/no_vowels.py
+++ 5.Lists_advanced/no_vowels.py
@@ -3,3 +3,15 @@ vowels = ['a', 'o', 'u', 'e', 'i']
 result = [letter for letter in text if letter.lower() not in vowels]
 print("".join(result))
 
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# text = input()
+# vowels = ['a', 'o', 'u', 'e', 'i']
+# result = ""
+# for letter in text:
+#     if letter.lower() not in vowels:
+#         result += letter
+#
+# print(result)
+#
