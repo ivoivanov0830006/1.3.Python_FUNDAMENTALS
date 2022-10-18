e614a95207703de717e8f0fa627cc68bcc42f4c1
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-18 23:35:53 +0300

    Update inventory.py

--- 5.Lists_advanced/inventory.py
+++ 5.Lists_advanced/inventory.py
@@ -27,3 +27,30 @@ while cmd != "Craft!":
             journal.append(item)
     cmd = input()
 print(", ".join(journal))
+
+
+# --------------------------- Problem to resolve -----------------------------
+# As a young traveler, you gather items and craft new items.
+# Input / Constraints
+# You will receive a journal with some collecting items, separated with a comma and a space (", ").
+# After that, until receiving "Craft!" you will be receiving different commands split by " - ":
+#   * "Collect - {item}" - you should add the given item to your inventory. If the item already exists,
+# you should skip this line.
+#   * "Drop - {item}" - you should remove the item from your inventory if it exists.
+#   * "Combine Items - {old_item}:{new_item}" - you should check if the old item exists. If so, add the
+# new item after the old one. Otherwise, ignore the command.
+#   * "Renew – {item}" – if the given item exists, you should change its position and put it last in your
+# inventory.
+# Output
+# After receiving "Craft!" print the items in your inventory, separated by ", ".
+# Input	                            Output
+# Iron, Wood, Sword                 Iron, Sword, Gold
+# Collect - Gold
+# Drop - Wood
+# Craft!
+# ---------------------------------------------------------------------------
+# Iron, Sword                       Sword, Bow, Iron
+# Drop - Bronze
+# Combine Items - Sword:Bow
+# Renew - Iron
+# Craft!
