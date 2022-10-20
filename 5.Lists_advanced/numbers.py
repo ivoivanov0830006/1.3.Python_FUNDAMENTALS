bbfc4238afc8b2afc767327734cdaf197261b8b2
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-20 23:38:28 +0300

    Update numbers.py

--- 5.Lists_advanced/numbers.py
+++ 5.Lists_advanced/numbers.py
@@ -5,3 +5,19 @@ if len(new_list) < 1:
     print("No")
 new_list.sort(reverse=True)
 print(*new_list[:5], sep=" ")
+
+
+# Write a program to read a sequence of integers and find and print the top 5 numbers greater than the
+# average value in the sequence, sorted in descending order.
+# Input
+# Read from the console a single line holding space-separated integers.
+# Output
+# Print the above-described numbers on a single line, space-separated.
+# If less than 5 numbers hold the property mentioned above, print less than 5 numbers.
+# Print "No" if no numbers hold the above property.
+# Constraints
+# All input numbers are integers in the range [-1000000 … 1000000].
+# The count of numbers is in the range [1…10 000].
+# Examples
+# Input	                                    Output
+# 10 20 30 40 50	                        50 40
