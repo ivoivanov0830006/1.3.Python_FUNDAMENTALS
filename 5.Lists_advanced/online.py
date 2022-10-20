a93c800b0840db068d5ecbf4d00726664f909929
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-20 23:46:37 +0300

    Update online.py

--- 5.Lists_advanced/online.py
+++ 5.Lists_advanced/online.py
@@ -33,3 +33,35 @@ for room in all_rooms:
             break
 if not failed:
     print(f"You've made it!\nBitcoins: {bitcoins}\nHealth: {current_health}")
+
+    
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You have initial health 100 and initial bitcoins 0. You will be given a string representing the dungeon's
+# rooms. Each room is separated with '|' (vertical bar): "room1|room2|room3…"
+# Each room contains a command and a number, separated by space. The command can be:
+#   * "potion" - You are healed with the number in the second part. But your health cannot exceed your
+#       initial health (100). First print:
+#                       * "You healed for {amount} hp."
+#       After that, print your current health:
+#                       *"Current health: {health} hp."
+#   * "chest"
+#       You've found some bitcoins, the number in the second part. Print:
+#                       * "You found {amount} bitcoins."
+#   * In any other case, you are facing a monster, which you will fight. The second part of the room
+#       contains the attack of the monster. You should remove the monster's attack from your health.
+#       If you are not dead (health <= 0), you've slain the monster, and print:
+#                       * "You slayed {monster}."
+#       If you've died, print
+#                       * "You died! Killed by {monster}." and your quest is over.
+#       Print the best room you've manage to reach:
+#                       * "Best room: {room}"
+# If you managed to go through all the rooms in the dungeon, print on the following three lines:
+#                       * "You've made it!"
+#                       * "Bitcoins: {bitcoins}"
+#                       * "Health: {health}"
+# Input / Constraints
+# You receive a string representing the dungeon's rooms, separated with '|' (vertical bar):"room1|room2|room3…".
+# Output
+# Print the corresponding messages described above.
