4a387ffc22e1a450956a0969ef7388a0829d68ed
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-21 20:07:18 +0300

    Update bonus_scoring_system.py

--- 5.Lists_advanced/bonus_scoring_system.py
+++ 5.Lists_advanced/bonus_scoring_system.py
@@ -15,3 +15,26 @@ for student in range(1, students + 1):
 
 print(f"Max Bonus: {max_bonus}.")
 print(f"The student has attended {max_attendances} lectures.")
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# import math
+# students = int(input())
+# lectures = int(input())
+# bonus = int(input())
+# all_students = []
+# all_attendances = []
+#
+# for student in range(1, students + 1):
+#     attendances = int(input())
+#     total_bonus = math.ceil(attendances / lectures * (5 + bonus))
+#     all_students.append(total_bonus)
+#     all_attendances.append(attendances)
+#
+# max_bonus = max(all_students)
+# index = all_students.index(max_bonus)
+#
+# print(f"Max Bonus: {max_bonus}.")
+# print(f"The student has attended {all_attendances[index]} lectures.")
+
