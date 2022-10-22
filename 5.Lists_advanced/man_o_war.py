8e8dea4604259bbf3a1ef95a1c04b6d810c1df80
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-22 14:39:20 +0300

    Update man_o_war.py

--- 5.Lists_advanced/man_o_war.py
+++ 5.Lists_advanced/man_o_war.py
@@ -32,3 +32,41 @@ while True:
 
     else:
         print(f"Try again!")
+    
+    
+# -------------------------------- Problem to resolve -----------------------------
+#
+# On the first line, you will receive a sequence of elements. Each element in the sequence will have
+# a twin. Until the player receives "end" from the console, you will receive strings with two integers
+# separated by a space, representing the indexes of elements in the sequence.
+# If the player tries to cheat and enters two equal indexes or indexes which are out of bounds of the
+# sequence, you should add two matching elements at the middle of the sequence in the following format:
+# "-{number of moves until now}a"
+# Then print this message on the console:
+# "Invalid input! Adding additional elements to the board"
+# Input
+# On the first line, you will receive a sequence of elements
+# On the following lines, you will receive integers until the command "end"
+# Output
+# Every time the player hit two matching elements, you should remove them from the sequence and print
+# on the console the following message:
+# "Congrats! You have found matching elements - ${element}!"
+# If the player hit two different elements, you should print on the console the following message:
+# "Try again!"
+# If the player hit all matching elements before he receives "end" from the console, you should print
+# on the console the following message:
+# "You have won in {number of moves until now} turns!"
+# If the player receives "end" before he hits all matching elements, you should print on the console
+# the following message:
+# "Sorry you lose :(
+# {the current sequence's state}"
+# Constraints
+# All elements in the sequence will always have a matching element.
+# Input 	                        Output
+# 1 1 2 2 3 3 4 4 5 5               Congrats! You have found matching elements - 1!
+# 1 0                               Invalid input! Adding additional elements to the board
+# -1 0                              Congrats! You have found matching elements - 2!
+# 1 0                               Congrats! You have found matching elements - 3!
+# 1 0                               Congrats! You have found matching elements - -2a!
+# 1 0                               Sorry you lose :(
+# end	                            4 4 5 5
