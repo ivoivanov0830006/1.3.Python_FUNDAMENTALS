728e9a9557f57a21ec80d8b8561c633dd4ad5189
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-24 22:25:44 +0300

    Update tax_calculator.py

--- 2.Data_types_and_variables/tax_calculator.py
+++ 2.Data_types_and_variables/tax_calculator.py
@@ -38,38 +38,3 @@ print(f"The National Revenue Agency will collect {total_tax:.2f} euros in taxes.
 
 
 # ------------------------------------- Problem to resolve ------------------------------
-#
-# Write a program that calculates the needed provision for a quest in the woods.
-# First you will receive the days of the adventure, the count of the players and the group's
-# energy. Afterward you will receive provision for a day for one person:
-#           * Water
-#           * Food
-# The group calculates how many supplies they'd need for the adventure and takes that much water
-# and food.
-#   * Every day they chop wood an lose certain amount of energy. For each of the days you are going
-# to receive the amount of energy lost from chopping wood. The program should end if the energy
-# reaches 0 or less.
-#   * Every second day they drink water, which boosts their energy with 5% of their current energy
-# and at the same time drops their water supplies by 30% of their current water.
-#   # Every third day they eat, which reduces their food supplies (all food they have) be the
-# following amount: {currentFood} / {countOfPeople} and at the same time raises their group's
-# energy by 10%.
-# The copping of wood, the drinking of water, and the eating happen in the order above. If they
-# have enough energy to finish the quest, print the following message:
-#       "You are ready for the quest. You will be left with - {energyLevel} energy!"
-# If they run out of energy, print the following message and the food and water they were left
-# with before they ran out of energy:
-#       "You will run out of energy. You will be left with {food} food and {water} water."
-# Input / Constraints
-#   1st line: the days N of the adventure - an integer in the range[1...100]
-#   2nd line: the number of players - an integer in the range[1 - 1000]
-#   3rd line: the group's energy - a real number in the range[1 - 50000]
-#   4th line: water per day for one person - a real number[0.00 - 1000.00]
-#   5th line: food per day for one person - real number [0.00 - 1000.00]
-#   On the next N lines - one for each of the days - the amount of energy loss - a real number
-#   in the range [0.00 - 1000.00]
-#   You will always have enough food and water
-# The possible outputs are:
-#   * "You are ready for the quest. You will be left with - {energyLevel} energy!"
-#   * "You will run out of energy. You will be left with {food} food and {water} water."
-# The final numbers should be formatted to the second digit after decimal separator
