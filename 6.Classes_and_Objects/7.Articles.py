9803f357af1a8aba907b35ff4e927164a68d19d6
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-10-30 14:22:52 +0200

    Update 7.Articles.py

--- 6.Classes_and_Objects/7.Articles.py
+++ 6.Classes_and_Objects/7.Articles.py
@@ -26,3 +26,14 @@ article.edit("Syracuse, a city on the coast of the Italian island of Sicily, "
 article.rename("Temperature in Italy")
 article.change_author("B. T.")
 print(article)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Create a class called Article. The __init__ method should accept 3 arguments: title: str, content: str,
+# and author: str. The class should also have 4 methods:
+#   * edit(new_content: str) - changes the old content to the new one
+# change_author(new_author: str) - changes the old author with the new one
+# rename(new_title: str) - changes the old title with the new one
+# __repr__() - returns the following string "{title} - {content}: {author}"
+#
