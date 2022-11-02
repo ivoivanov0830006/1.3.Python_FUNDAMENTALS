ac1f3bde5608ef44e42bc20e770a6ddb2aa1cc64
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-02 22:54:18 +0200

    Update 1.Bakery.py

--- 7.Dictionaries/1.Bakery.py
+++ 7.Dictionaries/1.Bakery.py
@@ -15,3 +15,29 @@ for index in range(len(products)):
 
 bakery = {key_list[index]: value_list[index] for index in range(len(key_list))}
 print(bakery)
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# products = list(input().split())
+# bakery = {}
+#
+# key_list = []
+# value_list = []
+#
+#
+# for index in range(len(products)):
+#     if index % 2 != 0:
+#         value = int(products[index])
+#         value_list.append(value)
+#     else:
+#         key = products[index]
+#         key_list.append(key)
+#
+# for keys in key_list:
+#     for values in value_list:
+#         bakery[keys] = values
+#         value_list.remove(values)
+#         break
+#
+# print(bakery)
