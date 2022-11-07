5e0611089eae3eb06d683483f62bec75fc6da030
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:12:56 +0200

    Update 7.Word_synonyms.py

--- 7.Dictionaries/7.Word_synonyms.py
+++ 7.Dictionaries/7.Word_synonyms.py
@@ -13,3 +13,31 @@ while max_words > 0:
 for key, value in dictionary.items():
     value_list = ", ".join(value)
     print(f"{key} - {value_list}")
+
+    
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program, which keeps a dictionary with synonyms. The key of the dictionary will be the word.
+# The value will be a list of all the synonyms of that word. You will be given a number n – the count of
+# the words. After each term, you will be given a synonym, so the count of lines you should read from
+# the console is 2 * n. You will be receiving a word and a synonym each on a separate line like this:
+# ⦁	{word}
+# ⦁	{synonym}
+# If you get the same word twice, just add the new synonym to the list.
+# Print the words in the following format:
+# {word} - {synonym1, synonym2 … synonymN}
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                Output
+# 4                                     cute - adorable, charming
+# cute                                  smart - clever
+# adorable
+# cute
+# charming
+# smart
+# clever
+# -----------------------------------------------------------------
+# 2                                     task – problem, assignment
+# task
+# problem
+# task
+# assignment
