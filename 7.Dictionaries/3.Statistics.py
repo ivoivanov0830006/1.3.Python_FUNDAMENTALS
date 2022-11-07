3ba9dd884347881996e091b931e536ddaf8c3938
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:08:18 +0200

    Update 3.Statistics.py

--- 7.Dictionaries/3.Statistics.py
+++ 7.Dictionaries/3.Statistics.py
@@ -39,3 +39,34 @@ print(f"Total Quantity: {sum(products.values())}")
 #     print(f"- {product}: {quantity}")
 # print(f"Total Products: {len(statistics.items())}")
 # print(f"Total Quantity: {sum(statistics.values())}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will be receiving key-value pairs on separate lines separated by ": " until you receive the
+# command "statistics". Sometimes you may receive a product more than once. In that case, you have
+# to add the new quantity to the existing one. When you receive the "statistics" command, print the
+# following:
+# "Products in stock:
+# - {product1}: {quantity1}
+# - {product2}: {quantity2}
+# …
+# - {productN}: {quantityN}
+# Total Products: {count_all_products}
+# Total Quantity: {sum_all_quantities}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                        Output
+# bread: 4                      Products in stock:
+# cheese: 2                     - bread: 5
+# ham: 1                        - cheese: 2
+# bread: 1                      - ham: 1
+# statistics	                Total Products: 3
+#                               Total Quantity: 8
+# ----------------------------------------------------------------------------------------
+# eggs: 10                      Products in stock:
+# bread: 6                      - eggs: 10
+# cheese: 8                     - bread: 6
+# milk: 7                       - cheese: 8
+# statistics	                - milk: 7
+#                               Total Products: 4
+#                               Total Quantity: 31
