18e2bf0252e4f0079e3d7203bfdde125e8c89069
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:09:21 +0200

    Update 4.Students.py

--- 7.Dictionaries/4.Students.py
+++ 7.Dictionaries/4.Students.py
@@ -13,3 +13,24 @@ searched_course = command[0].replace("_", "")
 # print("\n".join(students[searched_course])
 for student in students[searched_course]:
     print(student)
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# students = {}
+# command = input()
+#
+# while ":" in command:
+#     current_command = command.split(":")
+#     name, id, course = current_command[0], current_command[1], current_command[2]
+#     if course not in students.keys():
+#         students[course] = {}
+#     students[course][id] = name
+#     command = input()
+#
+# course = " ".join(command.split("_"))
+#
+# for key, value in students.items():
+#     if key == course:
+#         for id, name in value.items():
+#             print(f"{name} - {id}")
