3f765201a296a449f52c53107cb7d2e7229e45a3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:07:57 +0200

    Update 3.Statistics.py

--- 7.Dictionaries/3.Statistics.py
+++ 7.Dictionaries/3.Statistics.py
@@ -16,3 +16,26 @@ for product, quantity in products.items():
 
 print(f"Total Products: {len(products)}")
 print(f"Total Quantity: {sum(products.values())}")
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# statistics = {}
+#
+# while True:
+#     command = input()
+#     if command == "statistics":
+#         break
+#     current_command = command.split(": ")
+#     food = current_command[0]
+#     pieces = int(current_command[1])
+#
+#     if food not in statistics.keys():
+#         statistics[food] = 0
+#     statistics[food] += pieces
+#
+# print("Products in stock:")
+# for product, quantity in statistics.items():
+#     print(f"- {product}: {quantity}")
+# print(f"Total Products: {len(statistics.items())}")
+# print(f"Total Quantity: {sum(statistics.values())}")
