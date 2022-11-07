70cae167b7f0b644d9967475b7c1d7b967a9c896
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:10:00 +0200

    Update 4.Students.py

--- 7.Dictionaries/4.Students.py
+++ 7.Dictionaries/4.Students.py
@@ -59,3 +59,24 @@ for student in students[searched_course]:
 #     value = courses[searched_course][index + 1]
 #     print(f"{key} - {value}")
 
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will be receiving names of students, their ID, and a course of programming they have taken
+# in the format "{name}:{ID}:{course}". On the last line, you will receive a name of a course in
+# snake case lowercase letters. You should print only the information of the students who have
+# taken the corresponding course in the format: "{name} - {ID}" on separate lines.
+# Note: each student's ID will always be unique
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                Output
+# Peter:123:programming basics          John - 5622
+# John:5622:fundamentals                Maya - 89
+# Maya:89:fundamentals                  Lilly - 633
+# Lilly:633:fundamentals
+# fundamentals
+# ----------------------------------------------------------------------------------------
+# Alex:6:programming basics             Alex - 6
+# Maria:7:programming basics            Maria - 7
+# Kaloyan:9:advanced
+# Todor:10:fundamentals
+# programming_basics
