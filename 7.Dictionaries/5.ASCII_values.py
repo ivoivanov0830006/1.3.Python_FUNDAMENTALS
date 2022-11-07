30bb9a24e8b3528a01242ecfd293d16c6ff1fe7d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:10:56 +0200

    Update 5.ASCII_values.py

--- 7.Dictionaries/5.ASCII_values.py
+++ 7.Dictionaries/5.ASCII_values.py
@@ -2,3 +2,13 @@ list_of_characters = input().split(", ")
 characters = {key: ord(key) for key in list_of_characters}
 print(characters)
 
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that receives a list of characters separated by ", ". It should create a dictionary with
+# each character as a key and its ASCII value as a value. Try solving that problem using comprehension.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                    Output
+# a, b, c, a	            {'a': 97, 'b': 98, 'c': 99}
+# ---------------------------------------------------------------------------------------
+# d, c, m, h	            {'d': 100, 'c': 99, 'm': 109, 'h': 104}
