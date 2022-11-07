802bb634fcb451d447f4d5f9fa2dbd122136aa14
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:09:43 +0200

    Update 4.Students.py

--- 7.Dictionaries/4.Students.py
+++ 7.Dictionaries/4.Students.py
@@ -34,3 +34,28 @@ for student in students[searched_course]:
 #     if key == course:
 #         for id, name in value.items():
 #             print(f"{name} - {id}")
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# courses = {}
+#
+# while True:
+#     command = list(map(str, input().split(":")))
+#     if len(command) == 1:
+#         break
+#     names_id = []
+#     name, identification, course = command[0], command[1], command[2]
+#     names_id.append(name)
+#     names_id.append(int(identification))
+#     if course not in courses.keys():
+#         courses[course] = []
+#     courses[course] += names_id
+#
+# searched_course = command[0].replace("_", " ")
+#
+# for index in range(0, len(courses[searched_course]), 2):
+#     key = courses[searched_course][index]
+#     value = courses[searched_course][index + 1]
+#     print(f"{key} - {value}")
+
