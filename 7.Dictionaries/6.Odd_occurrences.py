3607a6e91f25a8a194ffa262047c190b87aab172
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-07 19:11:56 +0200

    Update 6.Odd_occurrences.py

--- 7.Dictionaries/6.Odd_occurrences.py
+++ 7.Dictionaries/6.Odd_occurrences.py
@@ -14,3 +14,18 @@ for key in words_lowercase:
 for key, occurrences in words.items():
     if occurrences % 2 != 0:
         print(f"{key}", end=" ")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that prints all elements from a given sequence of words that occur an odd number of
+# times (case-insensitive) in it.
+# ⦁	Words are given on a single line, space-separated.
+# ⦁	Print the result elements in lowercase, in their order of appearance.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                Output
+# Java C# PHP PHP JAVA C java	        java c# c
+# -------------------------------------------------
+# 3 5 5 hi pi HO Hi 5 ho 3 hi pi	    5 hi
+# -------------------------------------------------
+# a a A SQL xx a xx a A a XX c	        a sql xx c
