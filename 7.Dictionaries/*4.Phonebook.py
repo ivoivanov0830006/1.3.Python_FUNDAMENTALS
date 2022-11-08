493d57eecb2bd6ac0c4a8276ae7f02dd1d1d4ead
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-08 19:41:49 +0200

    Update *4.Phonebook.py

--- 7.Dictionaries/*4.Phonebook.py
+++ 7.Dictionaries/*4.Phonebook.py
@@ -35,3 +35,28 @@ for check in range(int(entry)):
 #     else:
 #         print(f"Contact {person} does not exist.")
 
+
+# ------------------------------------- Problem to resolve ------------------------------
+# Write a program that receives info from the console about people and their phone numbers.
+#
+# Each entry should have a name and a number (both strings) separated by a "-". If you receive a name
+# that already exists in the phonebook, update its number.
+# After filling the phonebook, you will receive a number – N. Your program should be able to perform a
+# search of contact by name and print its details in the format: "{name} -> {number}". In case the
+# contact isn't found, print: "Contact {name} does not exist."
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                            Output
+# Adam-0888080808                   Contact Mery does not exist.
+# 2                                 Adam -> 0888080808
+# Mery
+# Adam
+# ------------------------------------------------------------------
+# Adam-+359888001122                Silvester -> 02/987665544
+# Ralf-666                          Contact silvester does not exist.
+# George-5559393                    Contact Rolf does not exist.
+# Silvester-02/987665544            Ralf -> 666
+# 4
+# Silvester
+# silvester
+# Rolf
+# Ralf
