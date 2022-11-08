f38aceec34c1bbb93cd73c50b299b4f49df3c36b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-08 19:41:31 +0200

    Update *4.Phonebook.py

--- 7.Dictionaries/*4.Phonebook.py
+++ 7.Dictionaries/*4.Phonebook.py
@@ -13,3 +13,25 @@ for check in range(int(entry)):
         print(f"{searched_name} -> {phonebook[searched_name]}")
     else:
         print(f"Contact {searched_name} does not exist.")
+
+        
+# ------------------------------------- Another Solution -----------------------------
+#
+# phonebook = {}
+#
+# while True:
+#     command = input().split("-")
+#     if command[0].isdigit():
+#         break
+#     name, number = command[0], command[1]
+#     phonebook[name] = number
+#
+# people_count = int(command[0])
+#
+# for _ in range(people_count):
+#     person = input()
+#     if person in phonebook.keys():
+#         print(f"{person} -> {phonebook[person]}")
+#     else:
+#         print(f"Contact {person} does not exist.")
+
