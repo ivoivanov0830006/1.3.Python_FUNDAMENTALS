4b83e2ea999ff842a1606ebb6e6e159cfe7c8410
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-08 19:38:43 +0200

    Update *3.Capitals.py

--- 7.Dictionaries/*3.Capitals.py
+++ 7.Dictionaries/*3.Capitals.py
@@ -5,3 +5,15 @@ final_result = {countries[index]: capitals[index] for index in range(len(countri
 
 for country, capital in final_result.items():
     print(f"{country} -> {capital}")
+    
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# countries = input().split(", ")
+# capitals = input().split(", ")
+#
+# final_result = dict(zip(countries, capitals))
+#
+# for country, capital in final_result.items():
+#     print(f"{country} -> {capital}")
+
