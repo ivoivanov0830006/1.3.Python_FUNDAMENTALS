fd3daa076d0c6023f6d3f1439c106c81f00a8a0a
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-08 19:39:03 +0200

    Update *3.Capitals.py

--- 7.Dictionaries/*3.Capitals.py
+++ 7.Dictionaries/*3.Capitals.py
@@ -16,4 +16,21 @@ for country, capital in final_result.items():
 #
 # for country, capital in final_result.items():
 #     print(f"{country} -> {capital}")
-
+#
+#
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Using a dictionary comprehension, write a program that receives country names on the first line,
+# separated by comma and space ", ", and their corresponding capital cities on the second line (again
+# separated by comma and space ", "). Print each country with their capital on a separate line in the
+# following format: "{country} -> {capital}".
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                    Output
+# Bulgaria, Romania, Germany, England       Bulgaria -> Sofia
+# Sofia, Bucharest, Berlin, London	        Romania -> Bucharest
+#                                           Germany -> Berlin
+#                                           England -> London
+# --------------------------------------------------------------
+# Bulgaria, Germany, France                 Bulgaria -> Varna
+# Varna, Frankfurt, Paris	                Germany -> Frankfurt
+#                                           France -> Paris
