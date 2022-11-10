e1db2e9e5edee4de3c2c35da7e5128214c7340d6
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-10 20:44:08 +0200

    Update 1.Reverse_strings.py

--- 8.Text_processing/1.Reverse_strings.py
+++ 8.Text_processing/1.Reverse_strings.py
@@ -3,3 +3,15 @@ while True:
     if input_word == "end":
         break
     print(f"{input_word} = {input_word[::-1]}")
+
+    
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that receives a single string. On the first line, print all the digits found in the
+# string, on the second – all the letters, and on the third – all the other characters. There will always
+# be at least one digit, one letter, and one other character.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	Output
+# Agd#53Dfg^&4F53	53453
+# AgdDfgF
+# #^&
