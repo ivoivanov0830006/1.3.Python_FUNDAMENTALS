16b9cc68fdf58b7545aeb27fb747f709253a19ae
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-10 20:44:33 +0200

    Update 2.Repeat_strings.py

--- 8.Text_processing/2.Repeat_strings.py
+++ 8.Text_processing/2.Repeat_strings.py
@@ -5,3 +5,19 @@ for word in words:
     new_word += word * len(word)
 
 print(new_word)
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# [print(word * len(word), end="") for word in input().split()]
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads a sequence of strings, separated by a single space.
+# Each string should be repeated N times, where N is the length of the string.
+# Print the final strings concatenated into one string.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                    Output
+# hi abc add	            hihiabcabcabcaddaddadd
+# work	                    workworkworkwork
+# ball	                    ballballballball
