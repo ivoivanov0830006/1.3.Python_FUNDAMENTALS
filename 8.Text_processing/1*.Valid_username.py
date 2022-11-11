a5e32862030c54f33528e04c53429419787bed17
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-11 23:20:04 +0200

    Update 1*.Valid_username.py

--- 8.Text_processing/1*.Valid_username.py
+++ 8.Text_processing/1*.Valid_username.py
@@ -14,3 +14,20 @@ for username in all_usernames:
             continue
         valid_username.append(username)
 print("\n".join(valid_username))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads usernames on a single line (separated by ", ") and prints all valid
+# usernames on separate lines.
+# A valid username:
+#   * has length between 3 and 16 characters inclusive
+#   * can contain only letters, numbers, hyphens, and underscores
+#   * has no redundant symbols before, after, or in between
+# -------------------------------------- Example inputs ----------------------------------
+# Input         	                                        Output
+# sh, too_long_username, !lleg@l ch@rs, jeffbutt	        jeffbutt
+# -------------------------------------------------------------------
+# Jeff, john45, ab, cd, peter-ivanov, @smith	            Jeff
+#                                                           John45
+#                                                           peter-ivanov
