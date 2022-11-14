38c94f29297d3c078575b3760a6df013c4e34bc7
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-14 18:15:44 +0200

    Update *2.Character_multiplier.py

--- 8.Text_processing/*2.Character_multiplier.py
+++ 8.Text_processing/*2.Character_multiplier.py
@@ -24,3 +24,18 @@ else:
         total += ord(string_1[index]) * ord(string_2[index])
 
 print(total)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Create a program that receives two strings on a single line separated by a single space.
+# Then, it prints the sum of their multiplied character codes as follows: multiply str1[0] with str2[0]
+# and add the result to the total sum, then continue with the next two characters. If one of the strings
+# is longer than the other, add the remaining character codes to the total sum without multiplication.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                    Output
+# George Peter	            52114
+# --------------------------------
+# 123 522	                7647
+# --------------------------------
+# a aaaa	                9700
