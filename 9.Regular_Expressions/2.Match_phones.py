1c09c3ea89492fd502771f2020006b21c7a5245b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-16 22:16:17 +0200

    Update 2.Match_phones.py

--- 9.Regular_Expressions/2.Match_phones.py
+++ 9.Regular_Expressions/2.Match_phones.py
@@ -8,3 +8,28 @@ phones = re.finditer(pattern, data)
 phones = [phone.group(0) for phone in phones]
 # print(*phones, sep=",")
 print(", ".join(phones))
+
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# A valid number has the following characteristics:
+#   ⦁	It starts with "+359"
+#   ⦁	Then, it is followed by the area code (always 2)
+#   ⦁	After that, it is followed by a number:
+#   ⦁	The number consists of 7 digits (separated into two groups of 3 and 4 digits, respectively).
+#   ⦁	The different parts are separated by either a space or a hyphen ('-').
+# You can use the following RegEx properties to help with the matching:
+#   ⦁	Use quantifiers to match a specific number of digits
+#   ⦁	Use a capturing group to make sure the delimiter is only one of the allowed characters (space or hyphen)
+# and not a combination of both (e.g., +359 2-111 111 has mixed delimiters, it is invalid). Use a group
+# back reference to achieve this.
+#   ⦁	Add a word boundary at the end of the match to avoid partial matches.
+#   ⦁	Ensure that there is a space before the '+' sign, or it is positioned at the beginning of the string.
+# You can use the following table of values to test your RegEx:
+# -------------------------------------- Example inputs ----------------------------------
+# Input
+# +359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222,
+# +359-2-222-22222 +359-2-222-2222
+# Output
+# +359 2 222 2222, +359-2-222-2222
