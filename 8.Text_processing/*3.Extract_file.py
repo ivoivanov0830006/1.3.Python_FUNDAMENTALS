c9a2dae4a5816222aa024c812087270cbf826493
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-17 21:36:28 +0200

    Update *3.Extract_file.py

--- 8.Text_processing/*3.Extract_file.py
+++ 8.Text_processing/*3.Extract_file.py
@@ -6,3 +6,15 @@ extension = searched_criteria[1]
 
 print(f"File name: {file_name}")
 print(f"File extension: {extension}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads the path to a file and subtracts the file name and its extension.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                                Output
+# C:\Internal\training-internal\Template.pptx	        File name: Template
+#                                                       File extension: pptx
+# ---------------------------------------------------------------------------
+# C:\Projects\Data-Structures\LinkedList.cs	            File name: LinkedList
+#                                                       File extension: cs
