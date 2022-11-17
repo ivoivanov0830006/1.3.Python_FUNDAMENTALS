001aaeda35991aa764504c7569407f93cb86f9d2
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-17 21:37:07 +0200

    Update *4.Caesar_cipher.py

--- 8.Text_processing/*4.Caesar_cipher.py
+++ 8.Text_processing/*4.Caesar_cipher.py
@@ -5,3 +5,15 @@ for char in text:
     new_text += chr(ord(char) + 3)
 
 print(new_text)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that returns an encrypted version of the same text. Encrypt the text by replacing
+# each character with the corresponding character three positions forward in the ASCII table.
+# For example, A would be replaced with D, B would become E, and so on. Print the encrypted text.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                        Output
+# Programming is cool!	        Surjudpplqj#lv#frro$
+# ----------------------------------------------------
+# One year has 365 days.	    Rqh#|hdu#kdv#698#gd|v1
