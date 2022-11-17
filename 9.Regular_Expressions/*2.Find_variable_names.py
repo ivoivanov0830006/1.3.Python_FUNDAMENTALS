66682088ccee1d2235a9a8f8d4b8f0ea94914afb
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-17 21:17:29 +0200

    Update *2.Find_variable_names.py

--- 9.Regular_Expressions/*2.Find_variable_names.py
+++ 9.Regular_Expressions/*2.Find_variable_names.py
@@ -10,3 +10,18 @@ for result in results:
     valid_strings.append(result[2])
 
 print(",".join(valid_strings))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that finds all variable names in each string. A variable name starts with an
+# underscore ("_") and contains only capital and non-capital letters and digits. Extract only their names
+# without the underscore. Try to do this only with regular expressions.
+# The output consists of all variable names extracted and printed on a single line, separated by a comma.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                                            Output
+# The _id and _age variables are both integers.	                    id,age
+# ----------------------------------------------------------------------------------------
+# Calculate the _area of the _perfectRectangle object.	            area,perfectRectangle
+# ----------------------------------------------------------------------------------------
+# __invalidVariable _evenMoreInvalidVariable_ _validVariable	    validVariable
