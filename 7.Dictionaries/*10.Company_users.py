3c374c6d5f44df837f9b44f046b05df40ca8f4bb
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-18 22:51:16 +0200

    Update *10.Company_users.py

--- 7.Dictionaries/*10.Company_users.py
+++ 7.Dictionaries/*10.Company_users.py
@@ -15,3 +15,38 @@ for key, value in company.items():
     print(f"{key}")
     for values in company[key]:
         print(f"-- {values}")
+
+        
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that keeps the information about companies and their employees.
+# You will be receiving company names and an employees' id until you receive the command "End" command.
+# Add each employee to the given company. Keep in mind that a company cannot have two employees with the
+# same id.
+# Print the company name and each employee's id in the following format:
+# "{company_name}
+# -- {id1}
+# -- {id2}
+# …
+# -- {idN}"
+# Input / Constraints
+# Until you receive the "End" command, you will be receiving input in the format:
+# "{company_name} -> {employee_id}".
+# The input always will be valid.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                            Output
+# SoftUni -> AA12345                SoftUni
+# SoftUni -> BB12345                -- AA12345
+# Microsoft -> CC12345              -- BB12345
+# HP -> BB12345                     Microsoft
+# End	                            -- CC12345
+#                                   HP
+#                                   -- BB12345
+# ---------------------------------------------
+# SoftUni -> AA12345                SoftUni
+# SoftUni -> CC12344                -- AA12345
+# Lenovo -> XX23456                 -- CC12344
+# SoftUni -> AA12345                Lenovo
+# Movement -> DD11111               -- XX23456
+# End	                            Movement
+#                                   -- DD11111
