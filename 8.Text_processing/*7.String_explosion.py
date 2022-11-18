be9b26c2df17f1b9367413f4b8740298c3146c34
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-18 22:47:58 +0200

    Update *7.String_explosion.py

--- 8.Text_processing/*7.String_explosion.py
+++ 8.Text_processing/*7.String_explosion.py
@@ -12,3 +12,24 @@ for index in range(len(some_text)):
         new_text += some_text[index]
 
 print(new_text)
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# explosion_list = input().split(">")
+#
+# new_list = [explosion_list[0]]
+# previous_count = 0
+#
+# for explosion in explosion_list:
+#     count = 0
+#     if explosion[0].isdigit():
+#         power = int(explosion[0]) + previous_count
+#         new_explosion = explosion[power:]
+#         new_list.append(new_explosion)
+#         previous_count = 0
+#         if power > len(explosion):
+#             count += power - len(explosion)
+#             previous_count = count
+#
+# print(">".join(new_list))
