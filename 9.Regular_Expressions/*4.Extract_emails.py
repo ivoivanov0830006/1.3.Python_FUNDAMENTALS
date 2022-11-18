14e3e8a5db6d68981ebbfdcea721796ad8d47aea
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-18 22:46:38 +0200

    Update *4.Extract_emails.py

--- 9.Regular_Expressions/*4.Extract_emails.py
+++ 9.Regular_Expressions/*4.Extract_emails.py
@@ -8,3 +8,15 @@ valid_mails = re.finditer(pattern, mails)
 for mail in valid_mails:
     group = mail.groupdict()
     print(f"{group['user']}@{group['host']}")
+
+    
+# ------------------------------------- Another Solution -----------------------------
+#
+# import re
+#
+# pattern = r"\s(([a-z0-9\.\-\_]+)@([a-z\-]+(\.[a-z]+)+)\b"
+# mails = input()
+#
+# valid_mails = re.findall(pattern, mails)
+# for email in emails:
+#     print(email[0])
