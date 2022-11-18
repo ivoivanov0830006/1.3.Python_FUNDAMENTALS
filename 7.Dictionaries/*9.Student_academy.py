3681becfe11c98e7cce7f3634fa135b1f5a4aaba
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-18 22:50:06 +0200

    Update *9.Student_academy.py

--- 7.Dictionaries/*9.Student_academy.py
+++ 7.Dictionaries/*9.Student_academy.py
@@ -17,3 +17,40 @@ for key, value in academy.items():
     average_grade = average(sum(value),len(value))
     if average_grade >= 4.5:
         print(f"{key} -> {average_grade:.2f}")
+
+        
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that keeps the information about students and their grades.
+# On the first line, you will receive an integer number representing the next pair of rows.
+# On the next lines, you will be receiving each student's name and their grade.
+# Keep track of all grades for each student and keep only the students with an average grade higher
+# than or equal to 4.50.
+# Print the final dictionary with students and their average grade in the following format:
+#       "{name} -> {averageGrade}"
+# Format the average grade to the 2nd decimal place.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                    Output
+# 5                         John -> 5.00
+# John                      Alice -> 4.50
+# 5.5                       George -> 5.00
+# John
+# 4.5
+# Alice
+# 6
+# Alice
+# 3
+# George
+# 5
+# ----------------------------------------------
+# 5                         Rob -> 5.50
+# Amanda                    Christian -> 5.00
+# 3.5                       Robert -> 6.00
+# Amanda
+# 4
+# Rob
+# 5.5
+# Christian
+# 5
+# Robert
+# 6
