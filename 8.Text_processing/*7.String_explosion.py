5dcb60018e3462f2d79939162d724a0917ebd609
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-18 22:48:15 +0200

    Update *7.String_explosion.py

--- 8.Text_processing/*7.String_explosion.py
+++ 8.Text_processing/*7.String_explosion.py
@@ -33,3 +33,25 @@ print(new_text)
 #             previous_count = count
 #
 # print(">".join(new_list))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads a string from the console that contains:
+# ⦁	Explosions marked with '>'
+# ⦁	Immediately after the mark, there will be an integer x, which signifies the strength of the explosion
+# ⦁	Any other characters
+# Your task is to delete x characters, starting after the exploded character ('>'). If you find another
+# explosion mark ('>') while deleting characters, you should add the strength to your previous explosion.
+# You should not delete the explosion character – '>'.
+# When all characters are processed, print the final string.
+# Constraints
+# ⦁	You will always receive strength for the explosions
+# ⦁	The path will consist only of letters from the Latin alphabet, integers, and the char '>'
+# ⦁	The strength of the punches will be in the interval [0…9]
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                Output
+# abv>1>1>2>2asdasd	                    abv>>>>dasd
+# -----------------------------------------------------------
+# pesho>2sis>1a>2akarate>4hexmaster	    pesho>is>a>karate>master
+
