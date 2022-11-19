42b0a30bc207476f0fc32339db0e956b11f18d9c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-19 21:06:41 +0200

    Update 4.The_imitation_game.py

--- 0.Exams_Fundamentals/4.The_imitation_game.py
+++ 0.Exams_Fundamentals/4.The_imitation_game.py
@@ -40,3 +40,37 @@ while True:
 
 
 print(f"The decrypted message is: {initial_text}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# On the first line of the input, you will receive the encrypted message. After that, until the "Decode" 
+# command is given, you will be receiving strings with instructions for different operations that need to 
+# be performed upon the concealed message to interpret it and reveal its true content. There are several 
+# types of instructions, split by '|'
+# "Move {number of letters}":
+#       Moves the first n letters to the back of the string
+# "Insert {index} {value}":
+#       Inserts the given value before the given index in the string
+# "ChangeAll {substring} {replacement}":
+#       Changes all occurrences of the given substring with the replacement text
+# Input / Constraints
+# On the first line, you will receive a string with a message.
+# On the following lines, you will be receiving commands, split by '|' .
+# Output
+# After the "Decode" command is received, print this message:
+# "The decrypted message is: {message}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                            Output
+# zzHe                              The decrypted message is: Hello
+# ChangeAll|z|l
+# Insert|2|o
+# Move|3
+# Decode
+# -------------------------------------------------------------------------
+# owyouh                            The decrypted message is: howareyou?
+# Move|2
+# Move|3
+# Insert|3|are
+# Insert|9|?
+# Decode
