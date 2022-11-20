f5f4094ac443e18cf2a3323e6f3c2cb939c1c9ba
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-20 12:42:19 +0200

    Update 5.Destination_mapper.py

--- 0.Exams_Fundamentals/5.Destination_mapper.py
+++ 0.Exams_Fundamentals/5.Destination_mapper.py
@@ -13,3 +13,27 @@ for match in matches:
 
 print(f"Destinations: {', '.join(valid_locations)}")
 print(f"Travel Points: {travel_points}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will be given a string representing some places on the map. You have to filter only the valid ones.
+# A valid location is:
+#       * Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
+#       * After the first "=" or "/" there should be only letters (the first must be upper-case, other letters
+#       could be upper or lower-case)
+#       * The letters must be at least 3
+# Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two
+# locations are valid.
+# After you have matched all the valid locations, you have to calculate travel points. They are calculated
+# by summing the lengths of all the valid destinations that you have found on the map.
+# In the end, on the first line, print:
+#       * "Destinations: {destinations joined by ', '}".
+#       * On the second line, print "Travel Points: {travel_points}".
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                                    Output
+# =Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=	    Destinations: Hawai, Cyprus
+#                                                           Travel Points: 11
+# --------------------------------------------------------------------------------------
+# ThisIs some InvalidInput	                                Destinations:
+#                                                           Travel Points: 0
