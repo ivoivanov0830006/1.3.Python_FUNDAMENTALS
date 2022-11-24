9bde7191a05f9e2e1e0dd67393fac8f79e98fea8
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-24 19:43:55 +0200

    Update 4.Secret_chat.py

--- 0.Exams_Fundamentals/4.Secret_chat.py
+++ 0.Exams_Fundamentals/4.Secret_chat.py
@@ -30,3 +30,37 @@ while True:
         print(final_message)
 
 print(f"You have a new text message: {final_message}")
+
+
+# ------------------------------------- Problem to resolve -----------------------------------------
+#
+# On the first line of the input, you will receive the concealed message. After that, until the "Reveal"
+# command is given, you will receive strings with instructions for different operations that need to be
+# performed upon the concealed message to interpret it and reveal its actual content. There are several types
+# of instructions, split by ":|:"
+#    * "InsertSpace:|:{index}":
+#       Inserts a single space at the given index. The given index will always be valid.
+#    * "Reverse:|:{substring}":
+#       If the message contains the given substring, cut it out, reverse it and add it at the end of the message.
+#       If not, print "error". This operation should replace only the first occurrence of the given substring
+#       if there are two or more occurrences.
+#    * "ChangeAll:|:{substring}:|:{replacement}":
+#       Changes all occurrences of the given substring with the replacement text.
+# After the "Reveal" command is received, print this message:
+#       "You have a new text message: {message}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                Output
+# heVVodar!gniV                         hellodar!gnil
+# ChangeAll:|:V:|:l                     hellodarling!
+# Reverse:|:!gnil                       hello darling!
+# InsertSpace:|:5                       You have a new text message: hello darling!
+# Reveal
+# ---------------------------------------------------------------------------------
+# Hiware?uiy                            Howare?uoy
+# ChangeAll:|:i:|:o                     Howareyou?
+# Reverse:|:?uoy                        error
+# Reverse:|:jd                          How areyou?
+# InsertSpace:|:3                       How are you?
+# InsertSpace:|:7                       You have a new text message: How are you?
+# Reveal
+#
