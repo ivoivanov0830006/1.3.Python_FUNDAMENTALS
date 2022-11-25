7cf226bf4b5c102a28dd619915d9998fa3b8db0b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-25 21:21:21 +0200

    Create 3.Numbers.py

---
+++ 0.Exams_Fundamentals/3.Numbers.py
@@ -0,0 +1,24 @@
+
+
+# --------------------------- Problem to resolve -----------------------------
+#
+# Write a program to read a sequence of integers and find and print the top 5 numbers greater than the
+# average value in the sequence, sorted in descending order.
+# Input
+# Read from the console a single line holding space-separated integers.
+# Output
+# Print the above-described numbers on a single line, space-separated.
+# If less than 5 numbers hold the property mentioned above, print less than 5 numbers.
+# Print "No" if no numbers hold the above property.
+# Constraints
+# All input numbers are integers in the range [-1000000 … 1000000].
+# The count of numbers is in the range [1…10 000].
+# Examples
+# Input	                                    Output
+# 10 20 30 40 50	                        50 40
+# -------------------------------------------------------------------
+# 5 2 3 4 -10 30 40 50 20 50 60 60 51	    60 60 51 50 50
+# -------------------------------------------------------------------
+# 1	                                        No
+# -------------------------------------------------------------------
+# -1 -2 -3 -4 -5 -6	                        -1 -2 -3
