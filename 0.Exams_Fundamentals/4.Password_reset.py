deab58ed3990bac685127a27e4d9fdcf788b74ee
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-25 20:55:20 +0200

    Update 4.Password_reset.py

--- 0.Exams_Fundamentals/4.Password_reset.py
+++ 0.Exams_Fundamentals/4.Password_reset.py
@@ -29,3 +29,37 @@ while True:
             print("Nothing to replace!")
 
 print(f"Your password is: {final_password}")
+
+
+# ------------------------------------- Problem to resolve -----------------------------------------
+#
+# Write a password reset program that performs a series of commands upon a predefined string. First, you 
+# will receive a string, and afterward, until the command "Done" is given, you will be receiving strings 
+# with commands split by a single space. The commands will be the following:
+#   * "TakeOdd"
+#  Takes only the characters at odd indices and concatenates them to obtain the new raw password and then prints it.
+#   * "Cut {index} {length}"
+# Gets the substring with the given length starting from the given index from the password and removes its 
+# first occurrence, then prints the password on the console.
+# The given index and the length will always be valid.
+#   * "Substitute {substring} {substitute}"
+# If the raw password contains the given substring, replaces all of its occurrences with the substitute 
+# text given and prints the result.
+# If it doesn't, prints "Nothing to replace!".
+# After the "Done" command is received, print:
+#       "Your password is: {password}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                            Output
+# Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr        icecream::hot::summer
+# TakeOdd                                           icecream::hot::mer
+# Cut 15 3                                          icecream-hot-mer
+# Substitute :: -                                   Nothing to replace!
+# Substitute | ^                                    Your password is: icecream-hot-mer
+# Done	
+# --------------------------------------------------------------------
+# up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy          programming!is!funny
+# TakeOdd                                           programming!is!fun
+# Cut 18 2                                          programming***is***fun
+# Substitute ! ***                                  Nothing to replace!            
+# Substitute ? .!.                                  Your password is: programming***is***fun
+# Done	
