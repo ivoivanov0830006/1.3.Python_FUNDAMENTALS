e63f5cefd88205fe5382cedf4646b65eb8894a0b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-25 20:24:29 +0200

    Update 5.Mirror_words.py

--- 0.Exams_Fundamentals/5.Mirror_words.py
+++ 0.Exams_Fundamentals/5.Mirror_words.py
@@ -32,3 +32,35 @@ else:
         print(", ".join(final_dict))
     else:
         print("No mirror words!")
+ 
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# import re
+# 
+# input_text = input()
+# pattern = r"(?P<sep>[#|@])(?P<word_one>[a-zA-Z]{3,})(?P=sep){2}(?P<word_two>[a-zA-Z]{3,})(?P=sep)"
+# matches = re.findall(pattern, input_text)
+# 
+# mirrored_words = []
+# valid_pairs_count = 0
+# 
+# for match in matches:
+#     valid_pairs_count += 1
+#     word_one = match[1]
+#     word_two = match[2]
+# 
+#     if word_one == word_two[::-1]:
+#         mirrored_words.append(f"{word_one} <=> {word_two}")
+# 
+# 
+# if valid_pairs_count == 0:
+#     print("No word pairs found!")
+#     print("No mirror words!")
+# else:
+#     print(f"{valid_pairs_count} word pairs found!")
+#     if not mirrored_words:
+#         print("No mirror words!")
+#     else:
+#         print("The mirror words are:")
+#         print(", ".join(mirrored_words))
