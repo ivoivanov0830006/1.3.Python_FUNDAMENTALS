967215abded549c3a4f9626b1ca6e04e95a6593c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-25 20:51:16 +0200

    Update 6.Heroes_of_code_and_logic.py

--- 0.Exams_Fundamentals/6.Heroes_of_code_and_logic.py
+++ 0.Exams_Fundamentals/6.Heroes_of_code_and_logic.py
@@ -65,3 +65,65 @@ if len(heroes) > 0:
         print(f"""{name}
   HP: {health}
   MP: {mana}""")
+
+        
+# ------------------------------------- Problem to resolve -----------------------------------------
+#
+# On the first line of the standard input, you will receive an integer n – the number of heroes that you can 
+# choose for your party. On the next n lines, the heroes themselves will follow with their hit points and 
+# mana points separated by a single space in the following format:
+#       "{hero name} {HP} {MP}"
+# HP stands for hit points and MP for mana points
+# a hero can have a maximum of 100 HP and 200 MP
+# After you have successfully picked your heroes, you can start playing the game. You will be receiving 
+# different commands, each on a new line, separated by " – ", until the "End" command is given.
+# There are several actions that the heroes can perform:
+#   * "CastSpell – {hero name} – {MP needed} – {spell name}"
+# If the hero has the required MP, he casts the spell, thus reducing his MP. Print this message:
+#       "{hero name} has successfully cast {spell name} and now has {mana points left} MP!"
+# If the hero is unable to cast the spell print:
+#       "{hero name} does not have enough MP to cast {spell name}!"
+#   * "TakeDamage – {hero name} – {damage} – {attacker}"
+# Reduce the hero HP by the given damage amount. If the hero is still alive (his HP is greater than 0) print:
+#       "{hero name} was hit for {damage} HP by {attacker} and now has {current HP} HP left!"
+# If the hero has died, remove him from your party and print:
+#       "{hero name} has been killed by {attacker}!"
+#   * "Recharge – {hero name} – {amount}"
+# The hero increases his MP. If it brings the MP of the hero above the maximum value (200), MP is increased 
+# to 200. (the MP can't go over the maximum value).
+#  Print the following message:
+#       "{hero name} recharged for {amount recovered} MP!"
+# "Heal – {hero name} – {amount}"
+# The hero increases his HP. If a command is given that would bring the HP of the hero above the maximum 
+# value (100), HP is increased to 100 (the HP can't go over the maximum value).
+#  Print the following message:
+#       "{hero name} healed for {amount recovered} HP!"
+# Print all members of your party who are still alive, in the following format (their HP/MP need to be 
+# indented 2 spaces):
+# "{hero name}
+#   HP: {current HP}
+#   MP: {current MP}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                    Output
+# 2                                         Solmyr healed for 10 HP!
+# Solmyr 85 120                             Solmyr recharged for 50 MP!
+# Kyrre 99 50                               Kyrre was hit for 66 HP by Orc and now has 33 HP left!
+# Heal - Solmyr - 10                        Kyrre has successfully cast ViewEarth and now has 35 MP!
+# Recharge - Solmyr - 50                    Solmyr
+# TakeDamage - Kyrre - 66 - Orc               HP: 95
+# CastSpell - Kyrre - 15 - ViewEarth          MP: 170
+# End	                                    Kyrre
+#                                             HP: 33
+#                                             MP: 35
+# ------------------------------------------------------------------------------------------
+# 4                                         SirMullich healed for 30 HP!
+# Adela 90 150                              Adela recharged for 50 MP!
+# SirMullich 70 40                          Tyris does not have enough MP to cast Fireball!
+# Ivor 1 111                                Tyris has been killed by Fireball!
+# Tyris 94 61                               Ivor has been killed by Mosquito!
+# Heal - SirMullich - 50                    Adela
+# Recharge - Adela - 100                      HP: 90
+# CastSpell - Tyris - 1000 - Fireball         MP: 200
+# TakeDamage - Tyris - 99 - Fireball        SirMullich
+# TakeDamage - Ivor - 3 - Mosquito            HP: 100
+# End	                                      MP: 40
