ffa23f64a4c18566a12100a9d848e328a5af40a0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-25 20:24:44 +0200

    Update 5.Mirror_words.py

--- 0.Exams_Fundamentals/5.Mirror_words.py
+++ 0.Exams_Fundamentals/5.Mirror_words.py
@@ -64,3 +64,43 @@ else:
 #     else:
 #         print("The mirror words are:")
 #         print(", ".join(mirrored_words))
+
+
+# ------------------------------------- Problem to resolve -----------------------------------------
+#
+# On the first line of the input, you will be given a text string. To win the competition, you have to find
+# all hidden word pairs, read them, and mark the ones that are mirror images of each other.
+# First of all, you have to extract the hidden word pairs. Hidden word pairs are:
+#   * Surrounded by "@" or "#" (only one of the two) in the following pattern #wordOne##wordTwo# or @wordOne@@wordTwo@
+#   * At least 3 characters long each (without the surrounding symbols)
+#   * Made up of letters only
+# If the second word, spelled backward, is the same as the first word and vice versa (casing matters!), they
+# are a match, and you have to store them somewhere. Examples of mirror words: #Part##traP# @leveL@@Level@ #sAw##wAs#
+# If you don`t find any valid pairs, print:
+#       "No word pairs found!"
+# If you find valid pairs print their count:
+#       "{valid pairs count} word pairs found!"
+# If there are no mirror words, print:
+#       "No mirror words!"
+# If there are mirror words print:
+#       "The mirror words are:
+#       {wordOne} <=> {wordtwo}, {wordOne} <=> {wordtwo}, … {wordOne} <=> {wordtwo}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input
+# @mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r
+# Output
+# 5 word pairs found!
+# The mirror words are:
+# Part <=> traP, leveL <=> Level, sAw <=> wAs
+# --------------------------------------------
+# Input
+# #po0l##l0op# @bAc##cAB@ @LM@ML@ #xxxXxx##xxxXxx# @aba@@ababa@
+# Output
+# 2 word pairs found!
+# No mirror words!
+# --------------------------------------------
+# Input
+# #lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#
+# Output	Comments
+# No word pairs found!
+# No mirror words!
