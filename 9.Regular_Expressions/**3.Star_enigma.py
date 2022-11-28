155997588bc5cdbfbea54305a0ba192fa2079b9c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-11-28 22:56:10 +0200

    Update **3.Star_enigma.py

--- 9.Regular_Expressions/**3.Star_enigma.py
+++ 9.Regular_Expressions/**3.Star_enigma.py
@@ -41,3 +41,43 @@ if len(destroyed_planets) > 0:
     for destroyed_planet in destroyed_planets:
         print(f"-> {destroyed_planet}")
 
+
+# ------------------------------------- Problem to resolve -------------------------------
+#
+# The war is in its peak, but you, young Padwan, can turn the tides with your programming skills. You are tasked
+# to create a program to decrypt the messages of The Order and prevent the death of hundreds of lives.
+# You will receive several messages, which are encrypted using the legendary star enigma. You should decrypt
+# the messages, following these rules:
+# To properly decrypt a message, you should count all the letters [s, t, a, r] – case insensitive and remove
+# the count from the current ASCII value of each symbol of the encrypted message.
+# After decryption:
+# Each message should have a planet name, population, attack type ('A', as attack or 'D', as destruction) and
+# soldier count.
+# The planet name starts after '@' and contains only letters from the Latin alphabet.
+# The planet population starts after ':' and is an Integer;
+# The attack type may be "A"(attack) or "D"(destruction) and must be surrounded by "!" (exclamation mark).
+# The soldier count starts after "->" and should be an Integer.
+# The order in the message should be: planet name -> planet population -> attack type -> soldier count.
+# Each part can be separated from the others by any character except: '@', '-', '!', ':' and '>'.
+# Input / Constraints
+# The first line holds n – the number of messages– integer in range [1…100];
+# On the next n lines, you will be receiving encrypted messages.
+# Output
+# After decrypting all messages, you should print the decrypted information in the following format:
+# First print the attacked planets, then the destroyed planets.
+# "Attacked planets: {attackedPlanetsCount}"
+# "-> {planetName}"
+# "Destroyed planets: {destroyedPlanetsCount}"
+# "-> {planetName}"
+# The planets should be ordered by name alphabetically.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                    Output
+# 2                                         Attacked planets: 1
+# STCDoghudd4=63333$D$0A53333               -> Alderaa
+# EHfsytsnhf?8555&I&2C9555SR                Destroyed planets: 1
+#                                           -> Cantonica
+# --------------------------------------------------------------
+# 3                                         Attacked planets: 0
+# tt(''DGsvywgerx>6444444444%H%1B9444       Destroyed planets: 2
+# GQhrr|A977777(H(TTTT                      -> Cantonica
+# EHfsytsnhf?8555&I&2C9555SR	            -> Coruscant
