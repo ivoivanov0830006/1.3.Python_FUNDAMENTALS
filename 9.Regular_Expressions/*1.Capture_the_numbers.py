a0773d123d77521aae7bf3d564dc11db81de8fae
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-01 20:38:10 +0200

    Update *1.Capture_the_numbers.py

--- 9.Regular_Expressions/*1.Capture_the_numbers.py
+++ 9.Regular_Expressions/*1.Capture_the_numbers.py
@@ -9,6 +9,20 @@ while True:
 
     for number in numbers:
         print(number.group(), end=" ")
+     
+    
+# ------------------------------------- Another Solution -----------------------------
+#
+# import re
+#
+# pattern = r'\d+'
+# line = input()
+# while line:
+#     matches = re.findall(pattern, line)
+#     if matches:
+#         print(" ".join(matches), end=" ")
+#
+#     line = input()
 
 
 # ------------------------------------- Problem to resolve ------------------------------
