d1d6d110794678b89705f44147888d8bd00992fb
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-01 20:39:49 +0200

    Update *5.Furniture.py

--- 9.Regular_Expressions/*5.Furniture.py
+++ 9.Regular_Expressions/*5.Furniture.py
@@ -19,3 +19,28 @@ print("Bought furniture:")
 for product in products:
     print(f"{product}")
 print(f"Total money spend: {total_sum:.2f}")
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# import re
+# products = {}
+#
+# while True:
+#     command = input()
+#     if command == "Purchase":
+#         break
+#
+#     pattern = r"^>>(?P<item>[A-Za-z]+)<<(?P<price>\d+\.?\d*)!(?P<quantity>\d+)"
+#     items = re.findall(pattern, command)
+#     if items:
+#         for item in items:
+#             key = item[0]
+#             value = float(item[1]) * int(item[2])
+#             products[key] = value
+#
+# print("Bought furniture:")
+# for product in products.keys():
+#     print(f"{product}")
+# print(f"Total money spend: {sum(products.values()):.2f}")
+
