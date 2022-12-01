fbf0a929d74cddb9e29e99bc30c57d87dac5ceb3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-01 20:40:03 +0200

    Update *5.Furniture.py

--- 9.Regular_Expressions/*5.Furniture.py
+++ 9.Regular_Expressions/*5.Furniture.py
@@ -44,3 +44,23 @@ print(f"Total money spend: {total_sum:.2f}")
 #     print(f"{product}")
 # print(f"Total money spend: {sum(products.values()):.2f}")
 
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that calculates the total cost of bought furniture. You will be given information about each
+# purchase on separate lines until you receive the command "Purchase". Valid information should be in the
+# format: ">>{furniture_name}<<{price}!{quantity}". The price could be a floating-point or integer number.
+# You should store the names of the furniture and the total price.
+# In the end, print the name of each bought furniture and the total cost, formatted to the second decimal point:
+# "Bought furniture:
+# {1st name}
+# {2nd name}
+# …
+# {N name}
+# Total money spend: {total_cost}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                        Output
+# >>Sofa<<312.23!3              Bought furniture:
+# >>TV<<300!5                   Sofa
+# >Invalid<<!5                  TV
+# Purchase	                    Total money spend: 2436.69
