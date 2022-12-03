bc1de9f42bb3c2b41a199807bdac753a1d5003ac
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-03 23:57:14 +0200

    Update 2.Shopping_list.py

--- 0.Exams_Fundamentals/2.Shopping_list.py
+++ 0.Exams_Fundamentals/2.Shopping_list.py
@@ -28,3 +28,36 @@ while True:
             initial_list.append(item)
 
 print(", ".join(initial_list))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will receive an initial list with groceries separated by an exclamation mark "!".
+# After that, you will be receiving 4 types of commands until you receive "Go Shopping!".
+# "Urgent {item}" - add the item at the start of the list.  If the item already exists, skip
+# this command.
+# "Unnecessary {item}" - remove the item with the given name, only if it exists in the list.
+# Otherwise, skip this command.
+# "Correct {oldItem} {newItem}" - if the item with the given old name exists, change its name
+# with the new one. Otherwise, skip this command.
+# "Rearrange {item}" - if the grocery exists in the list, remove it from its current position
+# and add it at the end of the list. Otherwise, skip this command.
+# Constraints
+# There won't be any duplicate items in the initial list
+# Output
+# Print the list with all the groceries, joined by ", ":
+# "{firstGrocery}, {secondGrocery}, … {nthGrocery}"
+# -------------------------------------- Example inputs ----------------------------------
+# Input                 	            Output
+# Tomatoes!Potatoes!Bread               Tomatoes, Potatoes, Bread
+# Unnecessary Milk
+# Urgent Tomatoes
+# Go Shopping!
+# --------------------------------------------------------------
+# Milk!Pepper!Salt!Water!Banana         Milk, Onion, Salt, Water, Banana
+# Urgent Salt
+# Unnecessary Grapes
+# Correct Pepper Onion
+# Rearrange Grapes
+# Correct Tomatoes Potatoes
+# Go Shopping!
