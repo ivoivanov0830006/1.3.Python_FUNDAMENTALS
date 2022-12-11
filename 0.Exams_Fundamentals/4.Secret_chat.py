f0f60f51f4d85f5e2b65f50116ba368a60a67728
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-11 15:30:36 +0200

    Update 4.Secret_chat.py

--- 0.Exams_Fundamentals/4.Secret_chat.py
+++ 0.Exams_Fundamentals/4.Secret_chat.py
@@ -44,57 +44,23 @@ while True:
 print(f"You have a new text message: {final_message}")
 
 
-concealed_message = input()
-final_message = concealed_message
-
-while True:
-    command = input()
-    if command == "Reveal":
-        break
-    current_command = command.split(":|:")
-    action = current_command[0]
-
-    if action == "InsertSpace":
-        index = int(current_command[1])
-        final_message = final_message[:index] + " " + final_message[index:]
-        print(final_message)
-
-    elif action == "Reverse":
-        substring = current_command[1]
-        if substring in final_message:
-            final_message = final_message.replace(substring, "", 1)
-            substring = substring[::-1]
-            final_message = final_message + substring
-            print(final_message)
-        else:
-            print("error")
-
-    elif action == "ChangeAll":
-        substring = current_command[1]
-        replacement = current_command[2]
-        final_message = final_message.replace(substring, replacement)
-        print(final_message)
-
-print(f"You have a new text message: {final_message}")
-
-
 # ------------------------------------- Another Solution -----------------------------
 #
 # concealed_message = input()
 # final_message = concealed_message
-# 
+#
 # while True:
 #     command = input()
 #     if command == "Reveal":
 #         break
 #     current_command = command.split(":|:")
 #     action = current_command[0]
-# 
+#
 #     if action == "InsertSpace":
 #         index = int(current_command[1])
 #         final_message = final_message[:index] + " " + final_message[index:]
 #         print(final_message)
-# 
+#
 #     elif action == "Reverse":
 #         substring = current_command[1]
 #         if substring in final_message:
@@ -104,13 +70,13 @@ print(f"You have a new text message: {final_message}")
 #             print(final_message)
 #         else:
 #             print("error")
-# 
+#
 #     elif action == "ChangeAll":
 #         substring = current_command[1]
 #         replacement = current_command[2]
 #         final_message = final_message.replace(substring, replacement)
 #         print(final_message)
-# 
+#
 # print(f"You have a new text message: {final_message}")
 
 
