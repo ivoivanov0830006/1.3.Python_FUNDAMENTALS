c14eb6e36b8eb8865eff309e268d466c10fe2cd6
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-11 15:29:56 +0200

    Update 4.Secret_chat.py

--- 0.Exams_Fundamentals/4.Secret_chat.py
+++ 0.Exams_Fundamentals/4.Secret_chat.py
@@ -1,3 +1,49 @@
+def insert_space_func(current_data):
+    global final_message
+    index = int(current_data[1])
+    final_message = final_message[:index] + " " + final_message[index:]
+    return final_message
+
+
+def reverse_func(current_data):
+    global final_message
+    substring = current_data[1]
+    if substring in final_message:
+        final_message = final_message.replace(substring, "", 1)
+        substring = substring[::-1]
+        final_message = final_message + substring
+        return final_message
+    return "error"
+
+
+def change_all_func(current_data):
+    global final_message
+    substring = current_data[1]
+    replacement = current_data[2]
+    final_message = final_message.replace(substring, replacement)
+    return final_message
+
+
+concealed_message = input()
+final_message = concealed_message
+
+while True:
+    command = input()
+    if command == "Reveal":
+        break
+    current_command = command.split(":|:")
+    action = current_command[0]
+
+    if action == "InsertSpace":
+        print(insert_space_func(current_command))
+    elif action == "Reverse":
+        print(reverse_func(current_command))
+    elif action == "ChangeAll":
+        print(change_all_func(current_command))
+
+print(f"You have a new text message: {final_message}")
+
+
 concealed_message = input()
 final_message = concealed_message
 
@@ -30,6 +76,42 @@ while True:
         print(final_message)
 
 print(f"You have a new text message: {final_message}")
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# concealed_message = input()
+# final_message = concealed_message
+# 
+# while True:
+#     command = input()
+#     if command == "Reveal":
+#         break
+#     current_command = command.split(":|:")
+#     action = current_command[0]
+# 
+#     if action == "InsertSpace":
+#         index = int(current_command[1])
+#         final_message = final_message[:index] + " " + final_message[index:]
+#         print(final_message)
+# 
+#     elif action == "Reverse":
+#         substring = current_command[1]
+#         if substring in final_message:
+#             final_message = final_message.replace(substring, "", 1)
+#             substring = substring[::-1]
+#             final_message = final_message + substring
+#             print(final_message)
+#         else:
+#             print("error")
+# 
+#     elif action == "ChangeAll":
+#         substring = current_command[1]
+#         replacement = current_command[2]
+#         final_message = final_message.replace(substring, replacement)
+#         print(final_message)
+# 
+# print(f"You have a new text message: {final_message}")
 
 
 # ------------------------------------- Problem to resolve -----------------------------------------
