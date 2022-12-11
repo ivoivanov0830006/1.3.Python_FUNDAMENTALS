07dac991091a84058372c7e3af3ddc31d34c8b91
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-11 21:24:49 +0200

    Update 4.World_tour.py

--- 0.Exams_Fundamentals/4.World_tour.py
+++ 0.Exams_Fundamentals/4.World_tour.py
@@ -1,30 +1,79 @@
+def add_stop_func(current_data):
+    global final_string
+    index = int(current_data[1])
+    new_stop = current_data[2]
+    if 0 <= index <= len(final_string) - 1:
+        final_string = final_string[:index] + new_stop + final_string[index:]
+        return final_string
+
+
+def remove_stop_func(current_data):
+    global final_string
+    start_index = int(current_data[1])
+    end_index = int(current_data[2])
+    if start_index >= 0 and end_index <= len(final_string) - 1:
+        final_string = final_string[:start_index] + final_string[end_index + 1:]
+        return final_string
+    return final_string
+
+
+def switch_func(current_data):
+    global final_string
+    old_location = current_data[1]
+    new_location = current_data[2]
+    if old_location in final_string:
+        final_string = final_string.replace(old_location, new_location)
+        return final_string
+
+
 initial_stops = input()
 final_string = initial_stops
 
-command = input()
-while command != "Travel":
+while True:
+    command = input()
+    if command == "Travel":
+        break
     current_command = command.split(":")
     action = current_command[0]
     if action == "Add Stop":
-        index = int(current_command[1])
-        new_stop = current_command[2]
-        if 0 <= index <= len(final_string) - 1:
-            final_string = final_string[:index] + new_stop + final_string[index:]
+        print(add_stop_func(current_command))
     elif action == "Remove Stop":
-        start_index = int(current_command[1])
-        end_index = int(current_command[2])
-        if start_index >= 0 and end_index <= len(final_string) - 1:
-            final_string = final_string[:start_index] + final_string[end_index + 1:]
+        print(remove_stop_func(current_command))
     elif action == "Switch":
-        old_location = current_command[1]
-        new_location = current_command[2]
-        if old_location in final_string:
-            final_string = final_string.replace(old_location, new_location)
-
-    print(final_string)
-    command = input()
+        print(switch_func(current_command))
 
 print(f"Ready for world tour! Planned stops: {final_string}")
+
+
+# ------------------------------------- Another Solution -----------------------------
+#
+# initial_stops = input()
+# final_string = initial_stops
+#
+# command = input()
+# while command != "Travel":
+#     current_command = command.split(":")
+#     action = current_command[0]
+#     if action == "Add Stop":
+#         index = int(current_command[1])
+#         new_stop = current_command[2]
+#         if 0 <= index <= len(final_string) - 1:
+#             final_string = final_string[:index] + new_stop + final_string[index:]
+#     elif action == "Remove Stop":
+#         start_index = int(current_command[1])
+#         end_index = int(current_command[2])
+#         if start_index >= 0 and end_index <= len(final_string) - 1:
+#             final_string = final_string[:start_index] + final_string[end_index + 1:]
+#     elif action == "Switch":
+#         old_location = current_command[1]
+#         new_location = current_command[2]
+#         if old_location in final_string:
+#             final_string = final_string.replace(old_location, new_location)
+#
+#     print(final_string)
+#     command = input()
+#
+# print(f"Ready for world tour! Planned stops: {final_string}")
 
 
 # ------------------------------------- Problem to resolve ------------------------------
