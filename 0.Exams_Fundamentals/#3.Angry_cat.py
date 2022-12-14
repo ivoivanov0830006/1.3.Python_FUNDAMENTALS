087dea6938f7f9cf86df4632219416f43ab1b503
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-14 23:00:43 +0200

    Update #3.Angry_cat.py

--- 0.Exams_Fundamentals/#3.Angry_cat.py
+++ 0.Exams_Fundamentals/#3.Angry_cat.py
@@ -58,3 +58,37 @@ else:
 #     print(f"Left - {total_sum_left}")
 # else:
 #     print(f"Right - {total_sum_right}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# A cat is breaking various household items. Each item has a price rating, a number that describes
+# how valuable that item is for John's owner. You will be given an entry point from which John
+# will break the items to his left and then to his right. John will never break the item at his
+# entry point. You must calculate the damage to both his left and right, then print only the
+# higher(bigger) damage to the household. If both sums are equal, print the left one.
+# Input/Constrains
+#       * On the first line, you will receive the price ratings, separated by(", "). Each element
+# will be an integer in the range[-2^31...2^31].
+#       * On the second line, you will receive the entry point, which will always be between the
+# second and the penultimate element in the array.
+#       * On the third line, you will receive the type of items that the cat wants to break, which
+# will be one of the following:
+#            - "cheap" - items that have a lower price rating than the entry point item
+#            - "expensive" - items that have the same price rating, or higher price rating than
+#           the entry point item.
+# Output
+#       * The possible output is: - "{position} - {sum of price ratings}"
+#       * Positions can be "Right" or "Left"
+# Input	                                        Output
+# 1, 5, 1                                       Left - 1
+# 1
+# cheap
+# --------------------------------------------------------
+# 5, 10, 12, 5, 4, 20                           Right - 4
+# 3
+# cheap
+# --------------------------------------------------------
+# -2, 2, 1, 5, 9, 3, 2, -2, 1, -1, -3, 3        Left - 20
+# 7
+# expensive
