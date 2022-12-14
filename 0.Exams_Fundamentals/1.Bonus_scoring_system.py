eb2d4bb04034cc3c9006856b41f24c9a42d144aa
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-14 22:56:02 +0200

    Update 1.Bonus_scoring_system.py

--- 0.Exams_Fundamentals/1.Bonus_scoring_system.py
+++ 0.Exams_Fundamentals/1.Bonus_scoring_system.py
@@ -37,3 +37,53 @@ print(f"The student has attended {max_attendances} lectures.")
 #
 # print(f"Max Bonus: {max_bonus}.")
 # print(f"The student has attended {all_attendances[index]} lectures.")
+
+
+# ----------------------------------- Problem to resolve -----------------------------
+#
+# Create a program that calculates bonus points for each student enrolled in a course.
+# On the first line, you are going to receive the number of the students. On the second line,
+# you will receive the total number of lectures in the course. The course has an additional bonus,
+# which you will receive on the third line. On the following lines, you will be receiving the
+# count of attendances for each student.
+# The bonus is calculated with the following formula:
+# {total bonus} = {student attendances} / {course lectures} * (5 + {additional bonus})
+# Find the student with the maximum bonus and print them, along with his attendances, in the
+# following format:
+# "Max Bonus: {max bonus points}."
+# "The student has attended {student attendances} lectures."
+# Round the bonus points at the end to the nearest larger number.
+# Input / Constrains
+# On the first line, you are going to receive the number of the students – an integer in the range [0…50]
+# On the second line, you will receive the number of the lectures – an integer number in the range [0...50].
+# On the third line, you will receive the additional bonus – an integer number in the range [0….100].
+# On the following lines, you will be receiving the attendance of each student.
+# There will never be students with equal bonuses.
+# Output
+# Print the maximum bonus points and the attendances of the given student, rounded to the nearest
+# larger number, scored by a student in this course in the format described above.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                Output
+# 5                     Max Bonus: 34.
+# 25                    The student has attended 24 lectures.
+# 30
+# 12
+# 19
+# 24
+# 16
+# 20
+# ------------------------------------------------------------
+# 10                    Max Bonus: 18.
+# 30                    The student has attended 28 lectures.
+# 14
+# 8
+# 23
+# 27
+# 28
+# 15
+# 17
+# 25
+# 26
+# 5
+# 18
+
