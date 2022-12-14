5cca3126a6a0e786d52bf5dadd68c98a29bc738d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-14 23:00:26 +0200

    Update #3.Angry_cat.py

--- 0.Exams_Fundamentals/#3.Angry_cat.py
+++ 0.Exams_Fundamentals/#3.Angry_cat.py
@@ -21,3 +21,40 @@ if total_right_sum <= total_left_sum:
     print(f"Left - {total_left_sum}")
 else:
     print(f"Right - {total_right_sum}")
+
+    
+# ------------------------------------- Another Solution -----------------------------
+#
+# price_ratings = list(map(int, input().split(", ")))
+# entry_point = int(input())
+# items_type = input()
+#
+# left_part = price_ratings[:entry_point]
+# right_part = price_ratings[entry_point + 1::]
+#
+# total_sum_left = 0
+# total_sum_right = 0
+#
+#
+# if items_type == "cheap":
+#     for price in left_part:
+#         if price < price_ratings[entry_point]:
+#             total_sum_left += price
+#
+#     for price in right_part:
+#         if price < price_ratings[entry_point]:
+#             total_sum_right += price
+#
+# elif items_type == "expensive":
+#     for price in left_part:
+#         if price >= price_ratings[entry_point]:
+#             total_sum_left += price
+#
+#     for price in right_part:
+#         if price >= price_ratings[entry_point]:
+#             total_sum_right += price
+#
+# if total_sum_right <= total_sum_left:
+#     print(f"Left - {total_sum_left}")
+# else:
+#     print(f"Right - {total_sum_right}")
