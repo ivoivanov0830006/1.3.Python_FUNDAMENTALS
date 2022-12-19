ff86515089df9bfc1d1cd18a159239196fe27748
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-19 19:51:07 +0200

    Update *6.String_pureness.py

--- 1.Basic_syntax/*6.String_pureness.py
+++ 1.Basic_syntax/*6.String_pureness.py
@@ -1,42 +1,51 @@
-for string in range(int(input())):
-    is_pure = True
-    text = input()
-    for char in range(len(text)):
-        if text[char] == "," or text[char] == "." or text[char] == "_":
-            is_pure = False
-            break
-        else:
-            continue
-    if is_pure:
-        print(f"{text} is pure.")
+command = input()
+wrong = False
+
+while command != "Welcome!":
+    if len(command) < 5:
+        print(f"{command} goes to Gryffindor.")
+    elif len(command) == 5:
+        print(f"{command} goes to Slytherin.")
+    elif len(command) == 6:
+        print(f"{command} goes to Ravenclaw.")
+    elif command == "Voldemort":
+        wrong = True
+        break
     else:
-        print(f"{text} is not pure!")
-        
-        
-# ------------------------------------ Another solution ---------------------------------
-#
-# for string in range(int(input())):
-#     text = input()
-#     if "," in text or "." in text or "_" in text:
-#         print(f"{text} is pure.")
-#     else:
-#         print(f"{text} is not pure!"
+        print(f"{command} goes to Hufflepuff.")
+    command = input()
+if not wrong:
+    print("Welcome to Hogwarts.")
+else:
+    print("You must not speak of that name!")
 
 
 # ------------------------------------- Problem to resolve ------------------------------
 #
-# You will be given number n. After that, you'll receive different strings n times.
-# Your task is to check if the given strings are pure, meaning that they do NOT consist
-# of any of the characters: comma ",", period ".", or underscore "_":
-#   - If a string is pure, print "{string} is pure."
-#   - Otherwise, print "{string} is not pure!"
+# Help out the sorting hat to sort the new students in the houses of Hogwarts.
+# You will be receiving names until the command "Welcome!". The length of each name
+# determines in which house the student is going:
+#   - If the name is less than 5 chars, the student is going into Gryffindor
+#           Print "{name} goes to Gryffindor."
+#   - If the name is exactly 5 chars, the student is going into Slytherin
+#           Print "{name} goes to Slytherin."
+#   - If the name is exactly 6 chars, the student is going into Ravenclaw
+#           Print "{name} goes to Ravenclaw."
+#   - If the name is more than 6 chars, the student is going into Hufflepuff
+#           Print "{name} goes to Hufflepuff."
+# While receiving names, if you receive "Voldemort", print "You must not speak of that name!"
+# and end the program. No more sorting for today!
+#   - If all students are sorted successfully:
+#           Print "Welcome to Hogwarts."
 # -------------------------------------- Example inputs ----------------------------------
-# Input                     Output
-# 2
-# pure string               pure string is pure.
-# not_pure_string	        not_pure_string is not pure!
-# ----------------------------------------------------------
-# 3
-# SoftUni                   SoftUni is pure.
-# 12345                     12345 is pure.
-# string.pureness	        string.pureness is not pure!
+# Input	                            Output
+# Harry                             Harry goes to Slytherin.
+# Ron                               Ron goes to Gryffindor.
+# Ginny                             Ginny goes to Slytherin.
+# Draco                             Draco goes to Slytherin.
+# Welcome!                          Welcome to Hogwarts.
+# -------------------------------------------------------------------
+# Luna                              Luna goes to Gryffindor.
+# Hermione                          Hermione goes to Hufflepuff.
+# Neville                           Neville goes to Hufflepuff.
+# Voldemort	                        You must not speak of that name!
