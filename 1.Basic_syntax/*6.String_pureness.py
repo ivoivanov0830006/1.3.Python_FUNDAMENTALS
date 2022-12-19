f0537873c502733179e15fd9ab68af4a235c0287
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-19 19:46:59 +0200

    Update *6.String_pureness.py

--- 1.Basic_syntax/*6.String_pureness.py
+++ 1.Basic_syntax/*6.String_pureness.py
@@ -13,7 +13,7 @@ for string in range(int(input())):
         print(f"{text} is not pure!")
         
         
-# --------------------------- Another solution ----------------------------
+# ------------------------------------ Another solution ---------------------------------
 #
 # for string in range(int(input())):
 #     text = input()
@@ -21,3 +21,22 @@ for string in range(int(input())):
 #         print(f"{text} is pure.")
 #     else:
 #         print(f"{text} is not pure!"
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will be given number n. After that, you'll receive different strings n times.
+# Your task is to check if the given strings are pure, meaning that they do NOT consist
+# of any of the characters: comma ",", period ".", or underscore "_":
+#   - If a string is pure, print "{string} is pure."
+#   - Otherwise, print "{string} is not pure!"
+# -------------------------------------- Example inputs ----------------------------------
+# Input                     Output
+# 2
+# pure string               pure string is pure.
+# not_pure_string	        not_pure_string is not pure!
+# ----------------------------------------------------------
+# 3
+# SoftUni                   SoftUni is pure.
+# 12345                     12345 is pure.
+# string.pureness	        string.pureness is not pure!
