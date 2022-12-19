aeeccbe7cd1f62d5b21542dead0ae746ba4ad584
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-19 19:54:38 +0200

    Update and rename 4*.Maximum_multiple.py to *4.Maximum_multiple.py

--- 1.Basic_syntax/*4.Maximum_multiple.py
+++ 1.Basic_syntax/*4.Maximum_multiple.py
@@ -5,15 +5,17 @@ for number in range(boundary, 0, -1):
     if number % divisor == 0:
         print(number)
         break
+
         
-# --------------------------- Another solution ----------------------------
+# ------------------------------------ Another solution --------------------------------
 #
 # divisor = int(input())
 # boundary = int(input())
-# 
-# result = boundary // divisor * divisor
 #
-# --------------------------- Problem to resolve ----------------------------
+# result = boundary // divisor * divisor
+
+
+# ------------------------------------- Problem to resolve ------------------------------
 #
 # On the first line, you will be given a positive number, which will serve as a divisor.
 # On the second line, you will receive a positive number that will be the boundary.
@@ -22,6 +24,7 @@ for number in range(boundary, 0, -1):
 #   - less than or equal to the given bound
 #   - greater than 0
 # Note: it is guaranteed that N is found.
+# -------------------------------------- Example inputs ----------------------------------
 # Input	                Output
 # 2
 # 7	                    6
