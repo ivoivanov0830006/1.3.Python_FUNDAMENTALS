feca8cb7cae5a90f1ece526519db8333aa74010a
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-19 19:53:17 +0200

    Update *10.Mutated_strings.py

--- 1.Basic_syntax/*10.Mutated_strings.py
+++ 1.Basic_syntax/*10.Mutated_strings.py
@@ -16,7 +16,7 @@ for _ in range(len(word_1)):
         continue
 
         
-# --------------------------- Another solution (REPLACE + SLICES) ----------------------------
+# ----------------------------------- Another solution ---------------------------------
 # word_1 = input()
 # word_2 = input()
 #
@@ -24,8 +24,9 @@ for _ in range(len(word_1)):
 #     if word_1[char] != word_2[char]:
 #         word_1 = word_1[:char] + word_1[char:char + 1].replace(word_1[char], word_2[char], 1) + word_1[char + 1:]
 #         print(word_1)
-#
-# --------------------------- Another solution (LIST) ----------------------------
+
+
+# ------------------------------------ Another solution ---------------------------------
 # word_1 = input()
 # word_2 = input()
 # new_list = list(word_1)
@@ -34,12 +35,14 @@ for _ in range(len(word_1)):
 #     if new_list[char] != word_2[char]:
 #         new_list[char] = word_2[char]
 #         print("".join(new_list))
-#
-# --------------------------- Problem to resolve ----------------------------
+
+
+# ------------------------------------- Problem to resolve ------------------------------
 #
 # You will be given two strings. Transform the first string into the second one, letter by letter,
 # starting from the first one. After each interaction, print the resulting string only if it is unique.
 # Note: the strings will have the same length.
+# -------------------------------------- Example inputs ----------------------------------
 # Input	                Output
 # bubble gu             tubble gum
 # turtle hum            turble gum
