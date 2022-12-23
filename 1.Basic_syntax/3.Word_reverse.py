76579e87b7dc73c276a73b1f35ecb2a5533a1e06
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-23 21:45:48 +0200

    Create 3.Word_reverse.py

---
+++ 1.Basic_syntax/3.Word_reverse.py
@@ -0,0 +1,28 @@
+word = input()
+
+print(word[::-1])
+
+
+# ----------------------------------- Another solution ----------------------------
+#
+# word = input()
+#
+# reversed_word = ""
+# for i in range(len(word) - 1, -1, -1):
+#     reversed_word += word[i]
+# print(reversed_word)
+
+
+# ----------------------------------- Another solution ----------------------------
+#
+# word = input()
+# word_1 = ""
+#
+# for i in range(len(word) - 1, -1, -1):
+#     word_1 += word[i]
+# print(word_1)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that receives a single word, reverses it, and prints it.
