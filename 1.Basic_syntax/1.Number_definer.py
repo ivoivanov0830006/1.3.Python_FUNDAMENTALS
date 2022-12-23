6fd4c373ee76e247542ea5c8f59effe8d36abaa2
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-23 21:44:04 +0200

    Update 1.Number_definer.py

--- 1.Basic_syntax/1.Number_definer.py
+++ 1.Basic_syntax/1.Number_definer.py
@@ -16,3 +16,20 @@ elif number < 0:
         print("negative")
     else:
         print("large negative")
+
+        
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads a floating-point number and:
+# ⦁	prints "zero" if the number is zero
+# ⦁	prints "positive" or "negative"
+# ⦁	adds "small" if the absolute value of the number is less than 1 and it is not 0,
+# or "large" if it exceeds 1 000 000
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                Output
+# 25	                positive
+# 0.7	                small positive
+# 435247392.921	        large positive
+# -0.005	            small negative
+# -103.21	            negative
+# -358583355123.001	    large negative
