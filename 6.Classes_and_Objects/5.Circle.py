533a77ea9b4da996b6cc4ed069f458924a421ee1
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-24 23:31:10 +0200

    Create 5.Circle.py

---
+++ 6.Classes_and_Objects/5.Circle.py
@@ -0,0 +1,24 @@
+class Circle:
+    __pi = 3.14  # _PROCTECTED MEMBERS   __PRIVATE MEMBERS ! ! ! ne se dostapva izvan clasa
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+        self.radius = diameter / 2
+
+    def calculate_circumference(self):
+        return 2 * Circle.__pi * self.radius
+
+    def calculate_area(self):
+        return Circle.__pi * self.radius ** 2
+
+    def calculate_area_of_sector(self, angle):
+        return Circle.__pi * self.radius * self.radius * (angle / 360)
+
+
+circle = Circle(10)
+angle = 5
+
+
+print(f"{circle.calculate_circumference():.2f}")
+print(f"{circle.calculate_area():.2f}")
+print(f"{circle.calculate_area_of_sector():.2f}")
