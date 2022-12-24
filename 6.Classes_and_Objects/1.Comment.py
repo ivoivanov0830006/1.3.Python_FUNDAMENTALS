c2a8f73709180e68d1e2393ada770a9e33dd71ba
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-24 22:36:52 +0200

    Update 1.Comment.py

--- 6.Classes_and_Objects/1.Comment.py
+++ 6.Classes_and_Objects/1.Comment.py
@@ -10,3 +10,18 @@ comment = Comment("user1", "I like this book")
 print(comment.username)
 print(comment.content)
 print(comment.likes)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Create a class with the name "Comment". The __init__ method should accept 3 parameters:
+# ⦁	username
+# ⦁	content
+# ⦁	likes (optional, 0 by default)
+# Use the exact names for your variables
+# Note: there is no input/output for this problem. Test the class yourself and submit only the class
+# Test Code	                                            Output
+# comment = Comment("user1", "I like this book")        user1
+# print(comment.username)                               I like this book
+# print(comment.content)                                0
+# print(comment.likes)
