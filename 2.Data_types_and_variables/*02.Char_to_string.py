e0551e2a18a833b7377b9c55b21eb28eacaa3a61
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-21 21:15:55 +0300

    Create *02.Char_to_string.py

---
+++ 2.Data_types_and_variables/*02.Char_to_string.py
@@ -0,0 +1,24 @@
+char1 = input()
+char2 = input()
+char3 = input()
+
+total_string = char1 + char2 + char3
+print(total_string)
+
+# --------------------------- Problem to resolve ----------------------------
+#
+# Write a function that receives 3 characters. Concatenate all the characters into one string
+# and print it on the console.
+# Examples
+# Input	                    Output
+# a                         abc
+# b
+# c
+# -------------------------------------
+# %                         %2o
+# 2
+# o
+# -------------------------------------
+# 1                         15p
+# 5
+# p
