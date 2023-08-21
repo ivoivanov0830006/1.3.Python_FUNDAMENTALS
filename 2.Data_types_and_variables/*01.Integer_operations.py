2f5873dcebe1b3d5fd4a784386d5f13c64e3c263
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-21 21:16:56 +0300

    Create *01.Integer_operations.py

---
+++ 2.Data_types_and_variables/*01.Integer_operations.py
@@ -0,0 +1,23 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+result = (int((num1+num2)/num3)) * num4
+print(result)
+
+# --------------------------- Problem to resolve ----------------------------
+#
+# Write a program that reads four integer args. It should add the first to the second number,
+# integer divide the sum by the third number, and multiply the result by the fourth number.
+# Print the final result.
+# Input	                    Output
+# 10                        30
+# 20
+# 3
+# 3
+# ----------------------------------------
+# 15                        42
+# 14
+# 2
+# 3
