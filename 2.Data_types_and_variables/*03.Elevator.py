97392df35f3357d276c99ccb250643a3a102d6f9
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-21 21:15:30 +0300

    Update *03.Elevator.py

--- 2.Data_types_and_variables/*03.Elevator.py
+++ 2.Data_types_and_variables/*03.Elevator.py
@@ -30,3 +30,20 @@ print(total_courses)
 # total_courses = math.ceil(total_people / capacity)
 #
 # print(total_courses)
+
+
+# --------------------------- Problem to resolve ----------------------------
+#
+# Calculate how many courses will be needed to elevate N persons by using an elevator with
+# a capacity of P persons. The input holds two lines: the number of people N and the
+# capacity P of the elevator.
+# Input         	Output                  Comments
+# 17                6                       5 courses * 3 people
+# 3		                                    + 1 course * 2 persons
+# -----------------------------------------------------------------------------------
+# 4                 1                       All the persons fit inside the elevator.
+# 5		                                    Only one course is needed.
+# ------------------------------------------------------------------------------------
+# 10                2	                    2 courses * 5 people
+# 5
+
