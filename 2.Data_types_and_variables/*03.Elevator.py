5207be005918a64b0d61ddd924956dda68f201df
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-21 21:15:13 +0300

    Update *03.Elevator.py

--- 2.Data_types_and_variables/*03.Elevator.py
+++ 2.Data_types_and_variables/*03.Elevator.py
@@ -5,3 +5,17 @@ total_courses = total_people // capacity
 if total_people % capacity != 0:
     total_courses += 1
 print(total_courses)
+
+
+# --------------------------- Another solution ----------------------------
+
+# total_people = int(input())
+# capacity = int(input())
+# total_courses = 0
+#
+# rest_people = total_people % capacity
+# if rest_people == 0:
+#     total_courses = total_people / capacity
+# else:
+#     total_courses = total_people // capacity + 1
+# print(int(total_courses))
