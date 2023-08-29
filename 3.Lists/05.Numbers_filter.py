e759167160da30e68a015333ece3c69bc79ff419
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-29 23:14:01 +0300

    Update 05.Numbers_filter.py

--- 3.Lists/05.Numbers_filter.py
+++ 3.Lists/05.Numbers_filter.py
@@ -85,4 +85,28 @@ elif command_line == "positive":
 # print(filtered)
 #
 #
+# --------------------------- Problem to resolve -----------------------------
 #
+# On the first line, you will receive a single number n. On the following n lines, you will
+# receive integers. After that, you will be given one of the following commands:
+# ⦁	even
+# ⦁	odd
+# ⦁	negative
+# ⦁	positive
+# Filter all the args that fit in the category (0 counts as a positive and even).
+# Finally, print the result.
+# Input	                    Output
+# 5                         [-2, 18, 998]
+# 33
+# 19
+# -2
+# 18
+# 998
+# even
+# -----------------------------------------------
+# 3                         [-4]
+# 111
+# -4
+# 0
+# negative
+
