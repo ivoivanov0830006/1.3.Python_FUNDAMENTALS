c64e4b5851a4f00fa614df483322ae343ff44061
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-09-05 22:10:08 +0300

    Create *01.Smallest_of_three_numbers.py

---
+++ 4.Functions/*01.Smallest_of_three_numbers.py
@@ -0,0 +1,28 @@
+def min_number(number1, number2, number3):
+    result = min(number1, number2, number3)
+    return result
+
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+
+print(min_number(num_1, num_2, num_3))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a function that receives three integer args and returns the smallest. Print the result on
+# the console. Use an appropriate name for the function.
+# Input	                        Output
+# 2                             2
+# 5
+# 3
+# -----------------------------------------------
+# 600                           123
+# 342
+# 123
+# -----------------------------------------------
+# 25                            4
+# 21
+# 4
